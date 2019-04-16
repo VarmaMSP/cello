9649a6888e31eb9baa3d696217f07a33907f5bb4
Pavan Varma <varmamsp111@gmail.com>
2019-04-16 15:50:11 +0530

    Add deps.

--- model/podcast.go
+++ model/podcast.go
@@ -1,13 +1,25 @@
 package model
 
+import (
+	"github.com/mmcdole/gofeed/rss"
+)
+
 type Podcast struct {
 	Id          int    `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	ImagePath   string `json:"image_path"`
-	Explicit    int    `json:"explicit"`
+	Language    string `json:"language"`
+	Explicit    int    `json:"explicit,omitempty"`
 	Author      string `json:"author,omitempty"`
 	Type        string `json:"type"`
+	Block       int    `json:"block,omitempty"`
+	Complete    int    `json:"complete,omitempty"`
+}
+type PodcastAdminDetails struct {
+	Link       string
+	OwnerName  string
+	OwnerEmail string
 }
 
 type PodcastFeedDetails struct {
@@ -19,8 +31,63 @@ type PodcastFeedDetails struct {
 	LatestEpisodePubDate string
 }
 
-type PodcastAdminDetails struct {
-	Link       string
-	OwnerName  string
-	OwnerEmail string
+func GetPodcast(feed *rss.Feed) (*Podcast, error) {
+	// https://help.apple.com/itc/podcasts_connect/#/itcb54353390
+	p := &Podcast{
+		Title:       feed.Title,
+		Description: feed.Description,
+		ImagePath:   feed.ITunesExt.Image,
+		Language:    feed.Language,
+		Explicit:    0,
+		Author:      feed.ITunesExt.Author,
+		Type:        "episodic",
+		Block:       0,
+		Complete:    0,
+	}
+
+	if p.Title == "" {
+		return nil, nil
+	}
+
+	if p.Description == "" && feed.ITunesExt.Summary != "" {
+		p.Description = feed.ITunesExt.Summary
+	} else {
+		return nil, nil
+	}
+
+	if p.ImagePath == "" && feed.ITunesExt.Image != "" {
+		p.ImagePath = feed.ITunesExt.Image
+	} else {
+		return nil, nil
+	}
+
+	if feed.ITunesExt.Explicit == "true" {
+		p.Explicit = 1
+	}
+
+	if feed.ITunesExt.Type == "serial" {
+		p.Type = "serial"
+	}
+
+	if feed.ITunesExt.Block == "true" {
+		p.Block = 1
+	}
+
+	if feed.ITunesExt.Complete == "true" {
+		p.Complete = 1
+	}
+
+	return p, nil
 }
+
+func GetPodcastAdminDetails(feed *rss.Feed) *PodcastAdminDetails {
+	return &PodcastAdminDetails{
+		Link:       feed.Link,
+		OwnerName:  feed.ITunesExt.Owner.Name,
+		OwnerEmail: feed.ITunesExt.Owner.Email,
+	}
+}
+
+// TODO
+// func GetPodcastFeedDetails(feedUrl string, headers http.Header) *PodcastFeedDetails {
+// }
