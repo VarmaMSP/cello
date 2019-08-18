dbbdf454b8f20f57bb3ed74d5fd15a9660c496b4
varmamsp <varmamsp111@gmail.com>
2019-08-18 16:27:46 +0530

    Use filenames instead of glob patterns for purgecss paths

--- ui-server/next.config.js
+++ ui-server/next.config.js
@@ -1,25 +1,24 @@
+const glob = require('glob')
 const withCss = require('@zeit/next-css')
 const withPurgeCss = require('next-purgecss')
 
-module.exports = withCss(
-  withPurgeCss(
+const purgeCssConfig = {
+  paths: () => [
+    ...glob.sync(`${__dirname}/pages/**/*.ts?(x)`, {nodir: true}), 
+    ...glob.sync(`${__dirname}/components/**/*.ts?(x)`, {nodir: true})
+  ],
+  extractors: [
     {
-      purgeCssPaths: [
-        'pages/**/*.tsx',
-        'components/**/*.tsx',
-      ],
-      purgeCss: {
-        extractors: [
-          {
-            extractor: class {
-              static extract(content) {
-                return content.match(/[\w-/:]+(?<!:)/g) || []
-              }
-            },
-            extensions: ['tsx']
-          }
-        ]
-      }
+      extractor: class {
+        static extract(content) {
+          return content.match(/[\w-/:]+(?<!:)/g) || []
+        }
+      },
+      extensions: ['ts', 'tsx']
     }
-  )
+  ]
+}
+
+module.exports = withCss(
+  withPurgeCss({purgeCss: purgeCssConfig})
 )
