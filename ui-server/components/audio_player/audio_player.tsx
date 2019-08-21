209da44dd67e4b86e1d588e9940dc430925ac43e
varmamsp <varmamsp111@gmail.com>
2019-08-21 14:27:02 +0530

    Update audio player component to choose between lg, sm, md version of audio player based on screen width

--- ui-server/components/audio_player/audio_player.tsx
+++ ui-server/components/audio_player/audio_player.tsx
@@ -1,25 +1,34 @@
 import React, { Component } from 'react'
-import Controls from './components/controls'
-import ProgressBar from './components/progress_bar'
 import { AudioState } from './components/utils'
+import AudioPlayerSmall from './audio_player_small'
+import AudioPlayerMedium from './audio_player_medium'
+import AudioPlayerLarge from './audio_player_large'
 
 interface Props {
+  podcast: string;
+  episode: string;
+  podcastId: string;
+  episodeId: string;
+  albumArt: string;
   audioSrc: string;
   audioType: string;
-  playWhenReady: boolean;
 }
 
 interface State {
+  screen: "none" | "sm" | "md" | "lg";
   duration: number;
   currentTime: number;
   audioState: AudioState;
+  expandOnMobile: boolean;
 }
 
 export default class AudioPlayer extends Component<Props, State> {
   state = {
+    screen: "none" as "none" | "sm" | "md" | "lg",
     duration: 0,
     currentTime: 0,
-    audioState: "LOADING" as AudioState,
+    audioState: "LOADING" as AudioState, 
+    expandOnMobile: true,
   }
 
   audio: HTMLAudioElement = undefined
@@ -60,6 +69,30 @@ export default class AudioPlayer extends Component<Props, State> {
     })
 
     this.audio.src = this.props.audioSrc
+
+    // window resize
+    window.addEventListener("resize", () => {
+      this.handleScreenResize()
+    })
+
+    this.handleScreenResize()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", () => {})
+  }
+
+  handleScreenResize = () => {
+    const screenWidth = window.innerWidth
+    if (screenWidth >= 1024) {
+      this.setState({screen: "lg"})
+      return
+    }
+    if (screenWidth >= 768) {
+      this.setState({screen: "md"})
+      return
+    }
+    this.setState({screen: "sm"})
   }
 
   handleActionButtonPress = () => {
@@ -87,18 +120,65 @@ export default class AudioPlayer extends Component<Props, State> {
   }
   
   render() {
-    const {currentTime, duration} = this.state
+    const {
+      screen, audioState,
+      currentTime, duration
+    } = this.state
+    
+    const {
+      podcast, episode,
+      podcastId, episodeId,
+      albumArt
+    } = this.props
 
-    return <div className="w-full px-8 mt-64">
-      <Controls 
-        audioState={this.state.audioState}
-        handleFastForward={this.handleFastForward}
-        handleActionButtonPress={this.handleActionButtonPress}/>
-      <ProgressBar
-        currentTime={currentTime}
+    if (screen === "sm") {
+      return <AudioPlayerSmall
+        podcast={podcast}
+        episode={episode}
+        podcastId={podcastId}
+        episodeId={episodeId}
+        albumArt={albumArt}
+        audioState={audioState}
         duration={duration}
+        currentTime={currentTime}
         handleSeek={this.handleSeek}
+        handleFastForward={this.handleFastForward}
+        handleActionButtonPress={this.handleActionButtonPress}
       />
-    </div>
+    }
+
+    if (screen === "md") {
+      return <AudioPlayerMedium
+        podcast={podcast}
+        episode={episode}
+        podcastId={podcastId}
+        episodeId={episodeId}
+        albumArt={albumArt}
+        audioState={audioState}
+        duration={duration}
+        currentTime={currentTime}
+        handleSeek={this.handleSeek}
+        handleFastForward={this.handleFastForward}
+        handleActionButtonPress={this.handleActionButtonPress}
+      />
+    }
+
+    if (screen === "lg") {
+      return <AudioPlayerLarge
+        podcast={podcast}
+        episode={episode}
+        podcastId={podcastId}
+        episodeId={episodeId}
+        albumArt={albumArt}
+        audioState={audioState}
+        duration={duration}
+        currentTime={currentTime}
+        handleSeek={this.handleSeek}
+        handleFastForward={this.handleFastForward}
+        handleActionButtonPress={this.handleActionButtonPress}
+      />
+    }
+
+    return <></>
   }
 }
