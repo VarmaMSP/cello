9c40afbc3d0d8f36e8f9444d8a0cac76bfe0548c
varmamsp <varmamsp111@gmail.com>
2019-08-22 10:42:01 +0530

    Add hover effects to sign-in button

--- ui-server/components/navbar_top/components/sign_in_button.tsx
+++ ui-server/components/navbar_top/components/sign_in_button.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import classnames from 'classnames'
 
 const SignInButton: React.SFC<{}> = () => (
-  <button className="md:w-24 w-20 h-8 lg:mt-2 bg-orange-600 text-white rounded-lg outline-none">
-    <p className="w-full text-sm font-semibold leading-loose text-center">SIGN IN</p>
+  <button className="md:w-24 w-20 h-8 lg:mt-2 bg-orange-600 hover:bg-orange-700 active:bg-orange-700 rounded-lg focus:outline-none focus:shadow-outline ">
+    <p className="text-sm text-white font-semibold leading-loose">SIGN IN</p>
   </button>
 )
 
