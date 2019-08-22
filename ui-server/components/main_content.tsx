ad4a0d14d39e60b79836c0e2f53553e32e549609
varmamsp <varmamsp111@gmail.com>
2019-08-22 09:29:11 +0530

    Create main_content component to wrap main content reponsively

---
+++ ui-server/components/main_content.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+  children: JSX.Element | JSX.Element[]
+}
+
+
+const MainContent: React.SFC<Props> = ({children}) => (
+  <div className="lg:pl-56 pt-16 pb-32 z-0">
+    {children}
+  </div>
+)
+
+export default MainContent
