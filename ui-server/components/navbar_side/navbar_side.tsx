b57f35f8a7b217fd1f35209c03940534adf25680
varmamsp <varmamsp111@gmail.com>
2019-08-22 10:41:18 +0530

    Chnage side navbar padding

--- ui-server/components/navbar_side/navbar_side.tsx
+++ ui-server/components/navbar_side/navbar_side.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MenuItem from './components/menu_item';
 
 const NavbarSide: React.SFC<{}> = () => {
-  return <div className="fixed left-0 top-0 lg:flex flex-col hidden h-screen w-56 px-6 bg-white border shadow z-50">
+  return <div className="fixed left-0 top-0 lg:flex flex-col hidden h-screen w-56 px-3 bg-white border shadow z-50">
     <h3 className="w-full mb-8 text-3xl font-bold text-indigo-700 leading-relaxed text-center select-none">
       phenopod
     </h3>
