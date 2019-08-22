6175e09ec67d5f3d16cfb6ed1e172f358d18f158
varmamsp <varmamsp111@gmail.com>
2019-08-22 10:42:31 +0530

    Dummy main content

--- ui-server/pages/index/index.tsx
+++ ui-server/pages/index/index.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 export default class extends React.Component {
   render() {    
     return (
-      <div className=".not-italic">
-        Hello Next.js
-      </div>
+      <>
+        <h3>Phenopod</h3>
+        <h3>Phenopod</h3>
+        <h3>Phenopod</h3>
+        <h3>Phenopod</h3>
+        <h3>Phenopod</h3>
+      </>
     )
   }
 }
