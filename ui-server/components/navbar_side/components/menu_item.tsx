3677d3f806516157a3bdfab32e01b22c1fa145a2
varmamsp <varmamsp111@gmail.com>
2019-08-22 10:41:38 +0530

    Improve menu item accesibility

--- ui-server/components/navbar_side/components/menu_item.tsx
+++ ui-server/components/navbar_side/components/menu_item.tsx
@@ -12,8 +12,10 @@ const MenuItem: React.SFC<Props> = ({icon, name, active}) => {
   const Icon = iconMap[icon]
 
   return <a href="#" className={classnames(
-    "flex items-center w-full",
-    {"text-gray-700": !active, "text-green-600": active}
+    "flex items-center w-full px-4 rounded-full",
+    {"text-gray-700": !active, "text-green-600": active},
+    {"hover:bg-gray-200": !active},
+    {"cursor-pointer": !active, "cursor-default": active}
   )}>
     <Icon className="w-5 h-5 mr-3 fill-current"/>
     <h4 className="capitalize text-base leading-loose tracking-wide">
