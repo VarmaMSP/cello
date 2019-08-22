e10ae142704005520d2d4edcb665daed58ff77f6
varmamsp <varmamsp111@gmail.com>
2019-08-22 10:41:02 +0530

    Refactor bottom navbar

--- ui-server/components/navbar_bottom/navbar_bottom.tsx
+++ ui-server/components/navbar_bottom/navbar_bottom.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import { iconMap } from '../icon';
 
-const NavbarBottom: React.SFC<{}> = () => (
-  <section className="flex justify-around h-full pt-1 pb-0 bg-white z-50"> 
-    <button className="w-20 text-green-600">
-        <svg className="h-5 w-auto mx-auto fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7.88 7.88l-3.54 7.78 7.78-3.54 3.54-7.78-7.78 3.54zM10 11a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg>
-        <h4 className="capitalize text-xs text-center leading-loose underline">discover</h4> 
+const NavbarBottom: React.SFC<{}> = () => {
+  const IconExplore = iconMap["explore"]
+  const IconFeed = iconMap["feed"]
+  const IconSubscriptions = iconMap["heart"]
+  const IconProfile = iconMap["user-solid-circle"]
+
+  return <section className="flex justify-around h-full pt-1 pb-0 bg-white z-50"> 
+    <button className="w-20 text-green-600 rounded-full focus:outline-none">
+      <IconExplore className="w-5 h-5 mx-auto fill-current"/>
+      <h4 className="capitalize text-xs text-center leading-loose">discover</h4> 
     </button>
-    <button className="w-20 text-gray-700">
-        <svg className="h-5 w-auto mx-auto fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 6V2c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h4zm2 0h4a2 2 0 0 1 2 2v4h4V2H8v4zM2 8v10h10V8H2z"/></svg>
-        <h4 className="capitalize text-xs text-center leading-loose">feed</h4> 
+    <button className="w-20 text-gray-700 rounded-full focus:outline-none">
+      <IconFeed className="w-5 h-5 mx-auto fill-current"/>
+      <h4 className="capitalize text-xs text-center leading-loose">feed</h4> 
     </button>
-    <button className="w-20 text-gray-700">
-        <svg className="h-5 w-auto mx-auto fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/></svg>
-        <h4 className="capitalize text-xs text-center leading-loose">subscriptions</h4> 
+    <button className="w-20 text-gray-700 rounded-full focus:outline-none">
+      <IconSubscriptions className="w-5 h-5 mx-auto fill-current"/>
+      <h4 className="capitalize text-xs text-center leading-loose">subscriptions</h4> 
     </button>
-    <button className="w-20 text-gray-700">
-        <svg className="h-5 w-auto mx-auto fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
-        <h4 className="capitalize text-xs text-center leading-loose">profile</h4> 
+    <button className="w-20 text-gray-700 rounded-full focus:outline-none">
+      <IconProfile className="w-5 h-5 mx-auto fill-current"/>
+      <h4 className="capitalize text-xs text-center leading-loose">profile</h4> 
     </button>
   </section>
-)
+}
 
 export default NavbarBottom
