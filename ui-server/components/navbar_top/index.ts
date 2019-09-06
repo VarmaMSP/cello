9192d20b1df2d14f83e2eae895a9f5149ac07a55
varmamsp <varmamsp111@gmail.com>
2019-09-07 00:36:35 +0530

    map navbar to redux

--- ui-server/components/navbar_top/index.ts
+++ ui-server/components/navbar_top/index.ts
@@ -1,3 +1,24 @@
-import NavbarTop from './navbar_top'
+import NavbarTop, { StateToProps, DispatchToProps } from './navbar_top'
+import { AppState } from '../../store'
+import { getSearchBarText } from '../../selectors/ui/search'
+import { Dispatch } from 'redux'
+import { AppActions, SEARCH_BAR_TEXT_CHANGE } from '../../types/actions'
+import { connect } from 'react-redux'
 
-export default NavbarTop
+function mapStateToProps(state: AppState): StateToProps {
+  return {
+    searchText: getSearchBarText(state),
+  }
+}
+
+function mapDispatchToProps(dispatch: Dispatch<AppActions>): DispatchToProps {
+  return {
+    searchTextChange: (text: string) =>
+      dispatch({ type: SEARCH_BAR_TEXT_CHANGE, text }),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(NavbarTop)
