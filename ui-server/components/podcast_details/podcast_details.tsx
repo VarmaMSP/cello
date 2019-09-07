467be2bca4cbc3be059673d911a536a81dd509ad
varmamsp <varmamsp111@gmail.com>
2019-09-08 05:11:42 +0530

    Use shiitake to truncate podcast description

--- ui-server/components/podcast_details/podcast_details.tsx
+++ ui-server/components/podcast_details/podcast_details.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Shiitake from 'shiitake'
 import { Podcast } from '../../types/app'
 
 export interface StateToProps {
@@ -11,22 +12,38 @@ export interface OwnProps {
 
 interface Props extends StateToProps, OwnProps {}
 
-const PodcastDetails: React.SFC<Props> = ({ podcast }) => (
-  <div className="flex mb-8">
-    <img
-      className="lg:h-56 lg:w-56 h-32 w-32 flex-none object-cover object-center rounded"
-      src={`http://localhost:8080/img/${podcast.id}p-500x500.jpg`}
-    />
-    <div className="flex flex-col lg:px-5 px-3">
-      <h2 className="text-2xl text-gray-900">{podcast.title}</h2>
-      <h3 className="text-lg text-gray-800 leading-relaxed">
-        {podcast.author}
-      </h3>
-      <span className="lg:block hidden mt-1 text-sm text-gray-800 ">
-        {podcast.description}
-      </span>
+const PodcastDetails: React.SFC<Props> = ({ podcast }) => {
+  const [descLines, setDescLines] = useState(2)
+
+  return (
+    <div className="flex mb-8">
+      <img
+        className="lg:h-56 lg:w-56 h-32 w-32 flex-none object-cover object-center rounded-lg border"
+        src={`http://localhost:8080/img/${podcast.id}p-500x500.jpg`}
+      />
+      <div className="flex flex-col lg:px-5 px-3">
+        <h2 className="text-2xl text-gray-900">{podcast.title}</h2>
+        <h3 className="text-lg text-gray-800 leading-relaxed">
+          {podcast.author}
+        </h3>
+        <Shiitake
+          lines={descLines}
+          throttleRate={200}
+          className="lg:block hidden mt-1 text-sm text-gray-800 "
+          overflowNode={
+            <span
+              onClick={() => setDescLines(descLines == 2 ? 1000 : 2)}
+              className="text-blue-700 cursor-pointer"
+            >
+              {` ...read more`}
+            </span>
+          }
+        >
+          {podcast.description}
+        </Shiitake>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default PodcastDetails
