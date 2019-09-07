198f9816c0e686a593278de86d488ff58be7d7dd
varmamsp <varmamsp111@gmail.com>
2019-09-08 05:11:05 +0530

    Remove margin on sign in button

--- ui-server/components/navbar_top/components/sign_in_button.tsx
+++ ui-server/components/navbar_top/components/sign_in_button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SignInButton: React.SFC<{}> = () => (
-  <button className="md:w-24 w-20 h-8 lg:mt-2 bg-orange-600 hover:bg-orange-700 active:bg-orange-700 rounded-lg focus:outline-none focus:shadow-outline ">
+  <button className="md:w-24 w-20 h-8 bg-orange-600 hover:bg-orange-700 active:bg-orange-700 rounded-lg focus:outline-none focus:shadow-outline ">
     <p className="text-sm text-white font-semibold leading-loose">SIGN IN</p>
   </button>
 )
