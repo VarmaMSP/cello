19ce1995ebfcc4460288082a85815caf127a3eb0
varmamsp <varmamsp111@gmail.com>
2019-09-08 06:33:34 +0530

    Humanize past date in episode list

--- ui-server/components/episode_list/episode_list.tsx
+++ ui-server/components/episode_list/episode_list.tsx
@@ -20,14 +20,15 @@ interface Props extends StateToProps, DispatchToProps, OwnProps {}
 const EpisodeList: React.SFC<Props> = ({ episodes, playEpisode }) => {
   return (
     <>
-      {episodes.map(({ id, title, duration }) => (
+      {episodes.map(({ id, title, duration, pubDate }) => (
         <div key={id}>
           <div className="flex justify-between my-2 mx-1 rounded-full hover:bg-gray-100">
             <div className="flex-auto">
               <h4 className="text-sm font-medium">{title}</h4>
               <span className="text-xs">
                 {Utils.humanizeDuration(duration)}
-                <span className="mx-2 font-extrabold">&middot;</span>2 days ago
+                <span className="mx-2 font-extrabold">&middot;</span>
+                {Utils.humanizePastDate(pubDate)}
               </span>
             </div>
             <ButtonWithIcon
--- ui-server/components/utils.ts
+++ ui-server/components/utils.ts
@@ -8,3 +8,38 @@ export function humanizeDuration(d: number): string {
   }
   return res
 }
+
+export function humanizePastDate(dateStr: string): string {
+  const now = new Date()
+  const pastDate = new Date(dateStr)
+
+  // return relative time if dateStr is older that i month
+  const msPerMinute = 60 * 1000
+  const msPerHour = msPerMinute * 60
+  const msPerDay = msPerHour * 24
+  const msPerMonth = msPerDay * 30
+  const elapsed = +now - +pastDate
+
+  if (elapsed < msPerMinute) {
+    return `${Math.round(elapsed / 1000)} seconds ago`
+  } else if (elapsed < msPerHour) {
+    return `${Math.round(elapsed / msPerMinute)} minutes ago`
+  } else if (elapsed < msPerDay) {
+    return `${Math.round(elapsed / msPerHour)} hours ago`
+  } else if (elapsed < msPerMonth) {
+    return `${Math.round(elapsed / msPerDay)} days ago`
+  }
+
+  if (pastDate.getFullYear() === now.getFullYear()) {
+    return pastDate.toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+    })
+  } else {
+    return pastDate.toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    })
+  }
+}
