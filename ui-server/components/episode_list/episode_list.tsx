7086688901c827965b8d2e279db9de502407c1f4
varmamsp <varmamsp111@gmail.com>
2019-09-08 06:45:35 +0530

    Style episode list - Remove horizontal rules - Remove hover on small viewports - Make episode title larger

--- ui-server/components/episode_list/episode_list.tsx
+++ ui-server/components/episode_list/episode_list.tsx
@@ -22,9 +22,11 @@ const EpisodeList: React.SFC<Props> = ({ episodes, playEpisode }) => {
     <>
       {episodes.map(({ id, title, duration, pubDate }) => (
         <div key={id}>
-          <div className="flex justify-between my-2 mx-1 rounded-full hover:bg-gray-100">
+          <div className="flex justify-between my-2 lg:px-6 sm:px-4 py-2 rounded-full lg:hover:bg-gray-200">
             <div className="flex-auto">
-              <h4 className="text-sm font-medium">{title}</h4>
+              <h4 className="text-sm text-base tracking-wide leading-relaxed ">
+                {title}
+              </h4>
               <span className="text-xs">
                 {Utils.humanizeDuration(duration)}
                 <span className="mx-2 font-extrabold">&middot;</span>
@@ -37,7 +39,6 @@ const EpisodeList: React.SFC<Props> = ({ episodes, playEpisode }) => {
               onClick={() => playEpisode(id)}
             />
           </div>
-          <hr className="my-3" />
         </div>
       ))}
     </>
