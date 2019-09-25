10889c431f9854b354af77a35d1b15b2bbb38c84
varmamsp <varmamsp111@gmail.com>
2019-09-25 16:31:40 +0530

    Remove podcast thumbnail component

--- ui-server/components/list_search_results/list_search_results.tsx
+++ ui-server/components/list_search_results/list_search_results.tsx
@@ -1,7 +1,8 @@
 import Grid from 'components/grid'
-import PodcastThumbnail from 'components/podcast_thumbnail'
+import Link from 'next/link'
 import React, { Component } from 'react'
 import { Podcast } from 'types/app'
+import { getImageUrl } from 'utils/dom'
 
 export interface StateToProps {
   podcasts: Podcast[]
@@ -19,7 +20,24 @@ export default class extends Component<Props> {
     return (
       <Grid totalRowSpacing={{ LG: 12, MD: 8, SM: 8 }} className="mb-3 pb-3">
         {podcasts.map((podcast) => (
-          <PodcastThumbnail key={podcast.id} podcast={podcast} />
+          <Link
+            href={{ pathname: '/podcasts', query: { podcastId: podcast.id } }}
+            as={`/podcasts/${podcast.id}`}
+            key={podcast.id}
+          >
+            <div className="w-full cursor-pointer">
+              <img
+                className="w-full h-auto flex-none object-contain rounded-lg border"
+                src={getImageUrl(podcast.id, 'md')}
+              />
+              <p className="text-xs tracking-wide leading-tight text-gray-800 mt-2 mb-1 line-clamp-2">
+                {podcast.title}
+              </p>
+              <p className="text-xs tracking-tigher text-gray-600 truncate">
+                {`by ${podcast.author}`}
+              </p>
+            </div>
+          </Link>
         ))}
       </Grid>
     )
