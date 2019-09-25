c9b2c06a4ae49005f8e400e91a74db2b8858f376
varmamsp <varmamsp111@gmail.com>
2019-09-25 16:55:50 +0530

    Preserve background scroll position when modal is opened and closed

--- ui-server/components/modal.tsx
+++ ui-server/components/modal.tsx
@@ -8,20 +8,11 @@ export interface Props {
 
 export default class Modal extends Component<Props> {
   componentDidMount() {
-    const scrollY = document.documentElement.style.getPropertyValue(
-      '--scroll-y',
-    )
-
-    document.body.style.position = 'fixed'
-    document.body.style.top = `-${scrollY}`
+    document.body.style['overflow'] = 'hidden'
   }
 
   componentWillUnmount() {
-    const scrollY = document.body.style.top
-
-    document.body.style.position = ''
-    document.body.style.top = ''
-    window.scrollTo(0, parseInt(scrollY || '0') * -1)
+    document.body.style['overflow'] = 'auto'
   }
 
   render() {
@@ -31,7 +22,7 @@ export default class Modal extends Component<Props> {
       <>
         <div
           className="fixed inset-0 bg-white z-10"
-          style={{ opacity: 0.99 }}
+          style={{ opacity: 0.97 }}
         />
         <div className="modal px-3 py-4 border md:border-2 md:border-gray-300 border-gray-400 shadow z-20">
           <div className="w-full h-5 relative">
