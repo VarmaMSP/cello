ec038fd08bdcf5e6f166db37e9bdc9f837e2af9c
varmamsp <varmamsp111@gmail.com>
2019-09-26 12:11:23 +0530

    fix bottom navbar alignment issues

--- ui-server/components/audio_player/components/navbar_bottom.tsx
+++ ui-server/components/audio_player/components/navbar_bottom.tsx
@@ -12,59 +12,53 @@ interface StateToProps {
   currentUrlPath: string
 }
 
-const MenuItem: React.SFC<{ icon: Icon; name: string; active?: boolean }> = ({
-  icon,
-  name,
-  active,
-}) => {
+const MenuItem: React.SFC<{
+  icon: Icon
+  name: string
+  href: string
+  active?: boolean
+}> = ({ icon, name, href, active }) => {
   const Icon = iconMap[icon]
   return (
-    <a>
-      <div className="w-20 text-center cursor-pointer">
-        <Icon
-          className={classNames('w-5 h-5 mx-auto fill-current', {
-            'text-gray-700': !active,
-            'text-green-600': active,
-          })}
-        />
-        <h4 className="capitalize text-sm leading-loose tracking-wide">
-          {name}
-        </h4>
-      </div>
-    </a>
+    <div className="w-1/3 flex-none text-center cursor-pointer">
+      <Link href={href} scroll={false}>
+        <a>
+          <Icon
+            className={classNames('w-5 h-5 mx-auto fill-current', {
+              'text-gray-700': !active,
+              'text-green-600': active,
+            })}
+          />
+          <h4 className="capitalize text-sm leading-loose tracking-wide">
+            {name}
+          </h4>
+        </a>
+      </Link>
+    </div>
   )
 }
 
 const NavbarBottom: React.SFC<StateToProps> = ({ currentUrlPath }) => {
   return (
-    <div className="flex justify-around h-full pt-2 pb-0 bg-white z-50">
-      <Link href="/" scroll={false}>
-        <div>
-          <MenuItem
-            icon="explore"
-            name="home"
-            active={currentUrlPath === '/'}
-          />
-        </div>
-      </Link>
-      <Link href="/feed" scroll={false}>
-        <div>
-          <MenuItem
-            icon="feed"
-            name="feed"
-            active={currentUrlPath === '/feed'}
-          />
-        </div>
-      </Link>
-      <Link href="/subscriptions" scroll={false}>
-        <div>
-          <MenuItem
-            icon="heart"
-            name="subscriptions"
-            active={currentUrlPath === '/subscriptions'}
-          />
-        </div>
-      </Link>
+    <div className="flex h-full pt-2 pb-0 bg-white z-50">
+      <MenuItem
+        icon="explore"
+        name="home"
+        href="/"
+        active={currentUrlPath === '/'}
+      />
+      <MenuItem
+        icon="feed"
+        name="feed"
+        href="/feed"
+        active={currentUrlPath === '/feed'}
+      />
+      <MenuItem
+        icon="heart"
+        name="subscriptions"
+        href="/subscriptions"
+        active={currentUrlPath === '/subscriptions'}
+      />
     </div>
   )
 }
