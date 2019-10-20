211b5c9683fc7c9a376e4c8a3043d589cd0de6a5
varmamsp <varmamsp111@gmail.com>
2019-10-20 05:58:58 +0530

    Do not show feed and subscription menu items when user is signed out

--- ui-server/components/navbar_side/navbar_side.tsx
+++ ui-server/components/navbar_side/navbar_side.tsx
@@ -22,37 +22,42 @@ const NavbarSide: React.SFC<StateToProps> = (props) => {
             <MenuItem icon="home" name="home" active={currentUrlPath === '/'} />
           </li>
         </Link>
-        <Link href="/feed" scroll={false}>
-          <li className="h-10 my-1">
-            <MenuItem
-              icon="feed"
-              name="feed"
-              active={currentUrlPath === '/feed'}
-            />
-          </li>
-        </Link>
-        <Link href="/subscriptions" scroll={false}>
-          <li className="h-10 my-1">
-            <MenuItem
-              icon="heart"
-              name="subscriptions"
-              active={currentUrlPath === '/subscriptions'}
-            />
-          </li>
-        </Link>
+        {userSignedIn && (
+          <>
+            <Link href="/feed" scroll={false}>
+              <li className="h-10 my-1">
+                <MenuItem
+                  icon="feed"
+                  name="feed"
+                  active={currentUrlPath === '/feed'}
+                />
+              </li>
+            </Link>
+            <Link href="/subscriptions" scroll={false}>
+              <li className="h-10 my-1">
+                <MenuItem
+                  icon="heart"
+                  name="subscriptions"
+                  active={currentUrlPath === '/subscriptions'}
+                />
+              </li>
+            </Link>
+          </>
+        )}
       </ul>
-      <hr className="my-4" />
-      {!userSignedIn ? (
-        <div className="w-full my-3">
-          <div className="mb-2 text-center text-sm text-gray-800 tracking-tighter">
-            Subscribe to podcasts, create playlists and much more
+
+      {!userSignedIn && (
+        <>
+          <hr className="my-4" />
+          <div className="w-full my-3">
+            <div className="mb-2 text-center text-sm text-gray-800 tracking-tighter leading-tight">
+              Subscribe to podcasts, create playlists and much more
+            </div>
+            <div className="w-4/5 h-8 mx-auto">
+              <ButtonSignin />
+            </div>
           </div>
-          <div className="w-4/5 h-8 mx-auto">
-            <ButtonSignin />
-          </div>
-        </div>
-      ) : (
-        <></>
+        </>
       )}
     </div>
   )
