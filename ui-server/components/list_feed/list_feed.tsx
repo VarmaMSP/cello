30204e31c5d6bae0865bf21ed41dc69bfe03aabf
varmamsp <varmamsp111@gmail.com>
2019-10-27 10:45:27 +0530

    Show proper text when no when no episodes are published in a certain duration

--- ui-server/components/list_feed/list_feed.tsx
+++ ui-server/components/list_feed/list_feed.tsx
@@ -2,7 +2,6 @@ import ButtonPlay from 'components/button_play'
 import ButtonShowMore from 'components/button_show_more'
 import EpisodeMeta from 'components/episode_meta'
 import Grid from 'components/grid'
-import isThisWeek from 'date-fns/isThisWeek'
 import isToday from 'date-fns/isToday'
 import isYesterday from 'date-fns/isYesterday'
 import { Episode } from 'types/app'
@@ -26,7 +25,6 @@ const ListFeed: React.SFC<Props> = (props) => {
   const feedList: { title: string; episodes: Episode[] }[] = [
     { title: 'Today', episodes: [] },
     { title: 'Yesterday', episodes: [] },
-    { title: 'This Week', episodes: [] },
     { title: 'Earlier', episodes: [] },
   ]
 
@@ -42,47 +40,49 @@ const ListFeed: React.SFC<Props> = (props) => {
       feedList[1].episodes.push(episode)
       continue
     }
-    if (isThisWeek(pubDate)) {
-      feedList[2].episodes.push(episode)
-      continue
-    }
-    feedList[3].episodes.push(episode)
+    feedList[2].episodes.push(episode)
   }
 
   return (
     <>
       {feedList.map(({ title, episodes }) => (
         <div key={title}>
-          <h1 className="text-xl text-gray-900 my-5">{title}</h1>
-          <Grid
-            cols={{ LG: 2, MD: 1, SM: 1 }}
-            classNameChild="flex my-2 lg:px-2 py-2 rounded-lg md:hover:bg-gray-200"
-            totalRowSpacing={{ LG: 2, MD: 10, SM: 0 }}
-          >
-            {episodes.map((episode) => (
-              <>
-                <img
-                  className="w-24 h-24 flex-none object-contain rounded-lg border cursor-default"
-                  src={getImageUrl(episode.podcastId, 'md')}
-                  onClick={() => showEpisodeModal(episode.id)}
-                />
-                <div className="flex-auto flex flex-col justify-between pl-3">
-                  <div>
-                    <h1
-                      className="text-sm leading-tight line-clamp-2 cursor-default"
-                      onClick={() => showEpisodeModal(episode.id)}
-                    >
-                      {episode.title}
-                    </h1>
-                    <div className="mt-2">
-                      <EpisodeMeta episodeId={episode.id} />
+          <h1 className="text-xl text-gray-900 mt-5 mb-2">{title}</h1>
+          {episodes.length > 0 ? (
+            <Grid
+              cols={{ LG: 2, MD: 1, SM: 1 }}
+              classNameChild="flex my-2 lg:px-2 py-2 rounded-lg md:hover:bg-gray-200"
+              totalRowSpacing={{ LG: 2, MD: 10, SM: 0 }}
+            >
+              {episodes.map((episode) => (
+                <>
+                  <img
+                    className="w-24 h-24 flex-none object-contain rounded-lg border cursor-default"
+                    src={getImageUrl(episode.podcastId, 'md')}
+                    onClick={() => showEpisodeModal(episode.id)}
+                  />
+                  <div className="flex-auto flex flex-col justify-between pl-3">
+                    <div>
+                      <h1
+                        className="text-sm leading-tight line-clamp-2 cursor-default"
+                        onClick={() => showEpisodeModal(episode.id)}
+                      >
+                        {episode.title}
+                      </h1>
+                      <div className="mt-2">
+                        <EpisodeMeta episodeId={episode.id} />
+                      </div>
                     </div>
+                    <ButtonPlay className="w-5" episodeId={episode.id} />
                   </div>
-                  <ButtonPlay className="w-5" episodeId={episode.id} />
-                </div>
-              </>
-            ))}
-          </Grid>
+                </>
+              ))}
+            </Grid>
+          ) : (
+            <p className="text-gray-600 text-sm tracking-wide">
+              {'No episodes published'}
+            </p>
+          )}
         </div>
       ))}
 
