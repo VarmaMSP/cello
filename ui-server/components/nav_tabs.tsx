2c3181778e1553f364ccf97c0368dc16bbfc1c43
varmamsp <varmamsp111@gmail.com>
2019-11-07 14:46:44 +0530

    Update NavTabs component

--- ui-server/components/nav_tabs.tsx
+++ ui-server/components/nav_tabs.tsx
@@ -1,27 +1,35 @@
 import classNames from 'classnames'
+import Link from 'next/link'
 import React from 'react'
 
-interface OwnProps {
-  tabs: string[]
-  active: string
-  onClick: (tab: string) => void
+interface Tab {
+  name: string
+  pathname: string
+  query: { [x: string]: number | string | boolean }
+  as: string
 }
 
-const NavTabs: React.FC<OwnProps> = ({ tabs, active, onClick }) => {
+interface OwnProps {
+  tabs: Tab[]
+  active: string
+}
+
+const NavTabs: React.FC<OwnProps> = ({ tabs, active }) => {
   return (
     <div className="flex mt-8 mb-4 ">
       {tabs.map((t) => (
-        <div
-          className={classNames('mr-4 px-3 py-1 text-sm rounded-full', {
-            'bg-green-300': t === active,
-            'bg-gray-200': t !== active,
-            'cursor-default': t === active,
-            'cursor-pointer': t !== active,
-          })}
-          onClick={() => onClick(t)}
-        >
-          {t}
-        </div>
+        <Link href={{ pathname: t.pathname, query: t.query }} as={t.as}>
+          <a
+            className={classNames('block mr-4 px-3 py-1 text-sm rounded-full', {
+              'bg-green-300': t.name === active,
+              'bg-gray-200': t.name !== active,
+              'cursor-default': t.name === active,
+              'cursor-pointer': t.name !== active,
+            })}
+          >
+            {t.name}
+          </a>
+        </Link>
       ))}
     </div>
   )
