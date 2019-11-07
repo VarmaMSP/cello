a7d9b0c74217e374ee34d6d79d979fa0626d91a1
varmamsp <varmamsp111@gmail.com>
2019-11-07 07:25:41 +0530

    Add onClick to NavTabs component

--- ui-server/components/nav_tabs.tsx
+++ ui-server/components/nav_tabs.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 interface OwnProps {
   tabs: string[]
   active: string
+  onClick: (tab: string) => void
 }
 
-const NavTabs: React.FC<OwnProps> = ({ tabs, active }) => {
+const NavTabs: React.FC<OwnProps> = ({ tabs, active, onClick }) => {
   return (
     <div className="flex mt-8 mb-4 ">
       {tabs.map((t) => (
@@ -14,7 +15,10 @@ const NavTabs: React.FC<OwnProps> = ({ tabs, active }) => {
           className={classNames('mr-4 px-3 py-1 text-sm rounded-full', {
             'bg-green-300': t === active,
             'bg-gray-200': t !== active,
+            'cursor-default': t === active,
+            'cursor-pointer': t !== active,
           })}
+          onClick={() => onClick(t)}
         >
           {t}
         </div>
