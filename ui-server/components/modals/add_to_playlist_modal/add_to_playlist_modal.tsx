a4491bc4510fa22d9e8010c7b7a5d5d70745e8d0
varmamsp <varmamsp111@gmail.com>
2019-11-16 17:36:46 +0530

    Use Formik in add_to_playlist modal

--- ui-server/components/modals/add_to_playlist_modal/add_to_playlist_modal.tsx
+++ ui-server/components/modals/add_to_playlist_modal/add_to_playlist_modal.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import { Formik } from 'formik'
+import React, { useEffect } from 'react'
 import { RequestState } from 'reducers/requests/utils'
 import { Playlist } from 'types/app'
 import ModalContainer from '../components/modal_container'
 import Overlay from '../components/overlay'
-import Select from './components/select'
 
 export interface StateToProps {
   playlists: Playlist[]
@@ -21,17 +21,15 @@ export interface OwnProps {
   episodeId: string
 }
 
-const AddToPlaylistModal: React.FC<
-  StateToProps & DispatchToProps & OwnProps
-> = ({
+const AddToPlaylistModal: React.FC<StateToProps &
+  DispatchToProps &
+  OwnProps> = ({
   playlists,
   closeModal,
   showCreatePlaylistModal,
   loadPlaylists,
   reqState,
 }) => {
-  const [selected, setSelected] = useState<string>('')
-
   useEffect(() => {
     loadPlaylists()
   }, [])
@@ -39,30 +37,67 @@ const AddToPlaylistModal: React.FC<
   return (
     <Overlay background="rgba(0, 0, 0, 0.8)">
       <ModalContainer handleClose={closeModal} closeUponClicking="CROSS">
-        <div className="flex flex-col h-full justify-around">
+        <div className="flex flex-col h-full">
           <h4 className="flex-none block text-lg mb-4">{'Add to Playlist'}</h4>
-          <div className="flex-auto">
-            {reqState.status === 'STARTED' ? (
-              'LOADING ...'
-            ) : (
-              <Select
-                options={playlists.map((p) => ({ id: p.id, display: p.title }))}
-                selected={selected}
-                handleSelect={setSelected}
-              />
-            )}
-          </div>
-          <div className="flex flex-none md:justify-end justify-center items-center mb-4">
-            <button
-              className="w-32 text-sm font-medium text-center text-purple-400 py-1 mr-6 border-2 border-purple-400 rounded-lg"
-              onClick={() => showCreatePlaylistModal()}
+          {reqState.status === 'STARTED' ? (
+            'LOADING ...'
+          ) : (
+            <Formik
+              initialValues={{ playlistIds: [] as string[] }}
+              validate={() => ({})}
+              onSubmit={(values) => {
+                console.log(values)
+              }}
             >
-              New Playlist
-            </button>
-            <button className="w-32 px-4 py-1 text-sm text-center text-gray-100 bg-purple-500 rounded-lg">
-              Add
-            </button>
-          </div>
+              {({ values, isSubmitting, handleSubmit, setFieldValue }) => (
+                <form
+                  className="block flex-1 flex flex-col h-full"
+                  onSubmit={handleSubmit}
+                >
+                  <div className="flex-1">
+                    {playlists.map(({ id, title }) => (
+                      <div className="mb-1">
+                        <label className="inline-flex items-center">
+                          <input
+                            type="checkbox"
+                            name="playlistIds"
+                            className="form-checkbox"
+                            checked={values.playlistIds.includes(id)}
+                            onChange={(e) =>
+                              e.target.checked
+                                ? setFieldValue('playlistIds', [
+                                    ...new Set([...values.playlistIds, id]),
+                                  ])
+                                : setFieldValue(
+                                    'playlistIds',
+                                    values.playlistIds.filter((x) => x != id),
+                                  )
+                            }
+                          />
+                          <span className="ml-2">{title}</span>
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex flex-none md:justify-end justify-center items-center mb-4">
+                    <button
+                      className="w-32 text-sm font-medium text-center text-purple-400 py-1 mr-6 border-2 border-purple-400 rounded-lg"
+                      onClick={() => showCreatePlaylistModal()}
+                    >
+                      New Playlist
+                    </button>
+                    <button
+                      type="submit"
+                      className="w-32 px-4 py-1 text-sm text-center text-gray-100 bg-purple-500 rounded-lg"
+                      disabled={isSubmitting}
+                    >
+                      Add
+                    </button>
+                  </div>
+                </form>
+              )}
+            </Formik>
+          )}
         </div>
       </ModalContainer>
     </Overlay>
