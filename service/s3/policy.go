18ca8a7e4d70b2b4b44f194e6c3921f626f70d67
varmamsp <varmamsp111@gmail.com>
2019-12-02 15:24:32 +0530

    Add public access policy to thumbnails bucket

---
+++ service/s3/policy.go
@@ -0,0 +1,56 @@
+package s3
+
+import "encoding/json"
+
+// AWS POLICY GENERATOR
+// https://awspolicygen.s3.amazonaws.com/policygen.html
+
+type AwsPolicy struct {
+	Id        string               `json:"Id"`
+	Version   string               `json:"Version"`
+	Statement []AwsPolicyStatement `json:"Statement"`
+}
+
+type AwsPolicyStatement struct {
+	Sid       string            `json:"Sid"`
+	Effect    string            `json:"Effect"`
+	Principal map[string]string `json:"Principal"`
+	Action    []string          `json:"Action"`
+	Resource  []string          `json:"Resource"`
+}
+
+const (
+	POLICY_VERSION       = "2012-10-17"
+	POLICY_ID_THUMBNAILS = "2-12-2019"
+)
+
+func getNewBucketPolicy(bucketName string) AwsPolicy {
+	switch bucketName {
+	case BUCKET_NAME_THUMBNAILS:
+		return AwsPolicy{
+			Id:      POLICY_ID_THUMBNAILS,
+			Version: POLICY_VERSION,
+			Statement: []AwsPolicyStatement{
+				AwsPolicyStatement{
+					Sid:       "PublicReadAccess",
+					Effect:    "Allow",
+					Principal: map[string]string{"AWS": "*"},
+					Action:    []string{"s3:GetObject"},
+					Resource:  []string{"arn:aws:s3:::thumbnails/*"},
+				},
+			},
+		}
+	default:
+		return AwsPolicy{}
+	}
+}
+
+func marshalPolicy(policy AwsPolicy) string {
+	x, _ := json.Marshal(policy)
+	return string(x)
+}
+
+func unmarshalPolicy(s string) (policy AwsPolicy) {
+	json.Unmarshal([]byte(s), &policy)
+	return
+}
--- service/s3/s3.go
+++ service/s3/s3.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	BUCKET_NAME_THUMBNAILS       = "thumbanil"
+	BUCKET_NAME_THUMBNAILS       = "thumbnails"
 	BUCKET_NAME_PHENOPOD_CHARTS  = "phenopod-charts"
 	BUCKET_NAME_CHARTABLE_CHARTS = "chartable-charts"
 )
@@ -22,24 +22,46 @@ func NewS3Client(config *model.Config) (*minio.Client, error) {
 		return nil, err
 	}
 
-	if err := createBucket(s3Client, BUCKET_NAME_THUMBNAILS); err != nil {
+	// Create Buckets
+	if err := createBucket(BUCKET_NAME_THUMBNAILS, s3Client); err != nil {
 		return nil, err
 	}
-	if err := createBucket(s3Client, BUCKET_NAME_PHENOPOD_CHARTS); err != nil {
+	if err := createBucket(BUCKET_NAME_PHENOPOD_CHARTS, s3Client); err != nil {
 		return nil, err
 	}
-	if err := createBucket(s3Client, BUCKET_NAME_CHARTABLE_CHARTS); err != nil {
+	if err := createBucket(BUCKET_NAME_CHARTABLE_CHARTS, s3Client); err != nil {
+		return nil, err
+	}
+
+	// Set Policy
+	if err := setBucketPolicy(BUCKET_NAME_THUMBNAILS, s3Client); err != nil {
 		return nil, err
 	}
 
 	return s3Client, err
 }
 
-func createBucket(client *minio.Client, bucketName string) error {
+func createBucket(bucketName string, client *minio.Client) error {
 	if found, err := client.BucketExists(bucketName); err != nil {
 		return err
 	} else if found {
 		return nil
 	}
 	return client.MakeBucket(bucketName, "us-east-1")
+}
+
+func setBucketPolicy(bucketName string, client *minio.Client) error {
+	policy, err := client.GetBucketPolicy(bucketName)
+	if err != nil {
+		return err
+	}
+
+	old := unmarshalPolicy(policy)
+	new := getNewBucketPolicy(bucketName)
+	if old.Id != new.Id {
+		if err := client.SetBucketPolicy(bucketName, marshalPolicy((new))); err != nil {
+			return err
+		}
+	}
+	return nil
 }
