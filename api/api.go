81fd0de88f3d2879ad97942fa09d8d31434666e9
varmamsp <varmamsp111@gmail.com>
2019-12-18 17:21:01 +0530

    Bind new routes to server

--- api/api.go
+++ api/api.go
@@ -11,10 +11,10 @@ import (
 )
 
 type Api struct {
-	app *app.App
+	App *app.App
 
-	server *http.Server
-	router *httprouter.Router
+	Server *http.Server
+	Router *httprouter.Router
 
 	SchedulerJob           model.Job
 	ImportPodcastJob       model.Job
@@ -26,21 +26,21 @@ type Api struct {
 func NewApi(config model.Config) (*Api, error) {
 	api := &Api{}
 
-	api.router = httprouter.New()
-	api.server = &http.Server{
+	api.Router = httprouter.New()
+	api.Server = &http.Server{
 		Addr:    "127.0.0.1:8081",
-		Handler: api.router,
+		Handler: api.Router,
 	}
 
 	app, err := app.NewApp(config)
 	if err != nil {
 		return nil, err
 	}
-	api.app = app
+	api.App = app
 
 	if config.Jobs.Scheduler.Enable {
-		api.app.Log.Info().Msg("Starting scheduler job...")
-		job, err := job.NewSchedulerJob(api.app, &config)
+		api.App.Log.Info().Msg("Starting scheduler job...")
+		job, err := job.NewSchedulerJob(api.App, &config)
 		if err != nil {
 			return nil, err
 		}
@@ -49,8 +49,8 @@ func NewApi(config model.Config) (*Api, error) {
 	}
 
 	if config.Jobs.ImportPodcast.Enable {
-		api.app.Log.Info().Msg("Starting import podcast job...")
-		job, err := job.NewImportPodcastJob(api.app, &config)
+		api.App.Log.Info().Msg("Starting import podcast job...")
+		job, err := job.NewImportPodcastJob(api.App, &config)
 		if err != nil {
 			return nil, err
 		}
@@ -59,8 +59,8 @@ func NewApi(config model.Config) (*Api, error) {
 	}
 
 	if config.Jobs.RefreshPodcast.Enable {
-		api.app.Log.Info().Msg("Starting refresh podcast job...")
-		job, err := job.NewRefreshPodcastJob(api.app, &config)
+		api.App.Log.Info().Msg("Starting refresh podcast job...")
+		job, err := job.NewRefreshPodcastJob(api.App, &config)
 		if err != nil {
 			return nil, err
 		}
@@ -69,8 +69,8 @@ func NewApi(config model.Config) (*Api, error) {
 	}
 
 	if config.Jobs.CreateThumbnail.Enable {
-		api.app.Log.Info().Msg("Starting create thumbnail job...")
-		job, err := job.NewCreateThumbnailJob(api.app, &config)
+		api.App.Log.Info().Msg("Starting create thumbnail job...")
+		job, err := job.NewCreateThumbnailJob(api.App, &config)
 		if err != nil {
 			return nil, err
 		}
@@ -79,8 +79,8 @@ func NewApi(config model.Config) (*Api, error) {
 	}
 
 	if config.Jobs.SyncPlayback.Enable {
-		api.app.Log.Info().Msg("Starting sync playback job...")
-		job, err := job.NewSyncPlaybackJob(api.app, &config)
+		api.App.Log.Info().Msg("Starting sync playback job...")
+		job, err := job.NewSyncPlaybackJob(api.App, &config)
 		if err != nil {
 			return nil, err
 		}
@@ -88,22 +88,14 @@ func NewApi(config model.Config) (*Api, error) {
 		go api.SyncEpisodePlaybackJob.Run()
 	}
 
-	api.RegisterSearchHandlers()
-	api.RegisterPodcastHandlers()
-	api.RegisterSubscriptionHandlers()
-	api.RegisterEpisodeHandlers()
-	api.RegisterPlaybackHandlers()
-	api.RegisterHistoryHandlers()
-	api.RegisterPlaylistHandlers()
-	api.RegisterUserHandlers()
-	api.RegisterHomeHandlers()
+	api.RegisterHandlers()
 
 	return api, nil
 }
 
 func (api *Api) ListenAndServe() {
-	api.app.Log.Info().Msg("Server listening on port: 8081")
-	err := api.server.ListenAndServe()
+	api.App.Log.Info().Msg("Server listening on port: 8081")
+	err := api.Server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
