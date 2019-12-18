3d09b5a0466a9bccd85da07e6953cc037b6b189e
varmamsp <varmamsp111@gmail.com>
2019-12-18 17:17:36 +0530

    Modify  handler to take into account new context type

--- api/handler.go
+++ api/handler.go
@@ -1,88 +1,63 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/go-http-utils/headers"
-	"github.com/julienschmidt/httprouter"
 	"github.com/varmamsp/cello/app"
-	"github.com/varmamsp/cello/model"
 )
 
-type Context struct {
-	req     *http.Request
-	app     *app.App
-	session *model.Session
-	err     *model.AppError
-}
-
-func (c *Context) Query(key string) string {
-	return c.req.URL.Query().Get(key)
-}
-
-func (c *Context) Param(key string) string {
-	return httprouter.ParamsFromContext(c.req.Context()).ByName(key)
-}
-
-func (c *Context) Body() (m map[string]string) {
-	return model.MapFromJson(c.req.Body)
-}
-
-func (c *Context) DecodeBody(i interface{}) error {
-	decoder := json.NewDecoder(c.req.Body)
-	return decoder.Decode(i)
-}
-
 type Handler struct {
-	app            *app.App
-	handleFunc     func(*Context, http.ResponseWriter)
-	requireSession bool
+	App            *app.App
+	HandleFunc     func(*Context, http.ResponseWriter, *http.Request)
+	RequireSession bool
+}
+
+func (api *Api) H(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
+	return api.App.SessionManager.LoadAndSave(&Handler{
+		App:            api.App,
+		HandleFunc:     h,
+		RequireSession: false,
+	})
+}
+
+func (api *Api) HAuth(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
+	return api.App.SessionManager.LoadAndSave(&Handler{
+		App:            api.App,
+		HandleFunc:     h,
+		RequireSession: true,
+	})
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	c := &Context{
-		app:     h.app,
-		req:     req,
-		session: h.app.GetSession(req.Context()),
-	}
+	c := &Context{}
 
-	c.app.Log.Info().
+	c.App = h.App
+	c.Params = ParamsFromRequest(req)
+	c.Session = h.App.GetSession(req.Context())
+
+	c.App.Log.Info().
 		Str("method", req.Method).
 		Str("path", req.URL.String()).
 		Str("user_agent", req.Header.Get(headers.UserAgent)).
 		Msg("")
 
-	if h.requireSession && c.session == nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
+	if h.RequireSession {
+		c.RequireSession()
 	}
-	h.handleFunc(c, w)
 
-	if c.err != nil {
-		c.app.Log.Error().
-			Str("from", c.err.Id).
-			Str("error", c.err.DetailedError).
+	if c.Err == nil {
+		h.HandleFunc(c, w, req)
+	}
+
+	if c.Err != nil {
+		c.App.Log.Error().
+			Str("from", c.Err.Id).
+			Str("error", c.Err.DetailedError).
 			Msg("")
 
-		w.WriteHeader(c.err.StatusCode)
-		w.Write([]byte(c.err.Error()))
+		w.WriteHeader(c.Err.StatusCode)
+		w.Write([]byte(c.Err.Error()))
 		return
 	}
-}
-
-func (api *Api) NewHandler(h func(*Context, http.ResponseWriter)) http.Handler {
-	return api.app.SessionManager.LoadAndSave(&Handler{
-		app:            api.app,
-		handleFunc:     h,
-		requireSession: false,
-	})
-}
-
-func (api *Api) NewHandlerSessionRequired(h func(*Context, http.ResponseWriter)) http.Handler {
-	return api.app.SessionManager.LoadAndSave(&Handler{
-		app:            api.app,
-		handleFunc:     h,
-		requireSession: true,
-	})
 }
