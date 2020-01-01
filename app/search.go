1aeead873506b4a9e4c48b6f8a8f5cb05e3de150
varmamsp <varmamsp111@gmail.com>
2020-01-02 02:59:19 +0530

    Modify podcast search query

--- app/search.go
+++ app/search.go
@@ -2,37 +2,41 @@ package app
 
 import (
 	"context"
-	"reflect"
+	"net/http"
 
-	"github.com/olivere/elastic"
+	"github.com/olivere/elastic/v7"
 	"github.com/varmamsp/cello/model"
 )
 
-func (app *App) SearchPodcasts(searchQuery string) ([]*model.Podcast, *model.AppError) {
+func (app *App) SearchPodcasts(searchQuery string, offset, limit int) ([]*model.Podcast, *model.AppError) {
 	results, err := app.ElasticSearch.Search().
 		Index("podcast").
-		Query(elastic.NewMultiMatchQuery(searchQuery, "title", "author")).
-		Size(100).
+		Query(elastic.NewMultiMatchQuery(searchQuery).
+			FieldWithBoost("title", 3).
+			Field("author")).
+		Highlight(elastic.NewHighlight().
+			PreTags("<span class=\"result-highlight\">").
+			PostTags("</span>").
+			Field("title"),
+		).
+		From(offset).
+		Size(limit).
 		Do(context.TODO())
+
 	if err != nil {
-		return nil, nil
+		return nil, model.NewAppError("app.search_podcasts", "no results", http.StatusInternalServerError, nil)
 	}
 
-	podcasts := []*model.PodcastIndex{}
-	for _, item := range results.Each(reflect.TypeOf(model.PodcastIndex{})) {
-		tmp, _ := item.(model.PodcastIndex)
-		podcasts = append(podcasts, &tmp)
+	if results.Hits == nil || results.Hits.Hits == nil || len(results.Hits.Hits) == 0 {
+		return []*model.Podcast{}, nil
 	}
 
-	podcasts_ := make([]*model.Podcast, len(podcasts))
-	for i, podcast := range podcasts {
-		id, _ := model.Int64FromHashId(podcast.Id)
-		podcasts_[i] = &model.Podcast{
-			Id:     id,
-			Title:  podcast.Title,
-			Author: podcast.Author,
-			Type:   podcast.Type,
+	podcasts := []*model.Podcast{}
+	for _, hit := range results.Hits.Hits {
+		tmp := &model.Podcast{}
+		if err := tmp.LoadFromESHit(hit); err == nil {
+			podcasts = append(podcasts, tmp)
 		}
 	}
-	return podcasts_, nil
+	return podcasts, nil
 }
