312acb929c58f1577f7adc875ec98759531577c2
varmamsp <varmamsp111@gmail.com>
2020-01-05 18:28:17 +0530

    Add ability to filter search results

--- ui-server/components/search_results_filter/search_results_filter.tsx
+++ ui-server/components/search_results_filter/search_results_filter.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import Router from 'next/router'
 import React from 'react'
 import { SearchResultType, SearchSortBy } from 'types/search'
 
@@ -11,13 +12,43 @@ export interface OwnProps {
   sortBy: SearchSortBy
 }
 
-const resultTypes: SearchResultType[] = ['episode', 'podcast']
-
 const SearchResultsFilter: React.FC<StateToProps & OwnProps> = (props) => {
+  const onResultTypeChange = (t: SearchResultType) => {
+    if (props.resultType !== t) {
+      Router.push(
+        {
+          pathname: '/results',
+          query: {
+            query: props.searchBarText,
+            resultType: t,
+            sortBy: props.sortBy,
+          },
+        },
+        `/results?query=${props.searchBarText}&type=${t}&sort_by=${props.sortBy}`,
+      )
+    }
+  }
+
+  const onSortByChange = (s: SearchSortBy) => {
+    if (props.sortBy !== s) {
+      Router.push(
+        {
+          pathname: '/results',
+          query: {
+            query: props.searchBarText,
+            resultType: props.resultType,
+            sortBy: s,
+          },
+        },
+        `/results?query=${props.searchBarText}&type=${props.resultType}&sort_by=${s}`,
+      )
+    }
+  }
+
   return (
-    <div className="flex mb-8">
-      <div className="flex flex-initial w-1/2 border-b">
-        {resultTypes.map((t) => (
+    <div className="flex items-center mb-8 justify-between">
+      <div className="flex flex-initial w-3/5 border-b">
+        {(['episode', 'podcast'] as SearchResultType[]).map((t) => (
           <div className="w-20 mr-2 text-center">
             <div
               className={classNames(
@@ -27,16 +58,28 @@ const SearchResultsFilter: React.FC<StateToProps & OwnProps> = (props) => {
                   'cursor-pointer': props.resultType !== t,
                 },
               )}
+              onClick={() => onResultTypeChange(t)}
             >
-              {t}
+              {`${t}s`}
             </div>
             <div
               className={classNames('h-1 w-20 rounded-full', {
-                'bg-yellow-600': props.resultType === t,
+                'bg-green-600': props.resultType === t,
               })}
             />
           </div>
         ))}
+      </div>
+      <div>
+        <select
+          className="form-select text-sm tracking-wider w-36"
+          onChange={(e) => onSortByChange(e.target.value as SearchSortBy)}
+        >
+          <option value="relevance">{'Relevance'}</option>
+          {props.resultType === 'episode' && (
+            <option value="publish_date">{'Date Published'}</option>
+          )}
+        </select>
       </div>
     </div>
   )
