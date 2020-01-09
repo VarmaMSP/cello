d3c3e4998aba6e48d5fc2a0c88c89096d67d9d49
varmamsp <varmamsp111@gmail.com>
2020-01-09 13:16:49 +0530

    Modify browser Results handler

--- api/results.go
+++ api/results.go
@@ -85,27 +85,68 @@ func BrowseResults(c *Context, w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	podcastSearchResults, err := c.App.SearchPodcasts(c.Params.Query, c.Params.Offset, c.Params.Limit)
-	if err != nil {
-		c.Err = err
-		return
-	}
+	if c.Params.Type == "podcast" {
+		podcastSearchResults, err := c.App.SearchPodcasts(c.Params.Query, c.Params.Offset, c.Params.Limit)
+		if err != nil {
+			c.Err = err
+			return
+		}
 
-	podcastIds := make([]int64, len(podcastSearchResults))
-	for i, podcastSearchResult := range podcastSearchResults {
-		podcastIds[i] = podcastSearchResult.Id
-	}
-	podcasts, err := c.App.GetPodcastsByIds(podcastIds)
-	if err != nil {
-		c.Err = err
-		return
-	}
+		podcastIds := make([]int64, len(podcastSearchResults))
+		for i, podcastSearchResult := range podcastSearchResults {
+			podcastIds[i] = podcastSearchResult.Id
+		}
+		podcasts, err := c.App.GetPodcastsByIds(podcastIds)
+		if err != nil {
+			c.Err = err
+			return
+		}
 
-	w.Header().Set(headers.CacheControl, "private, max-age=3600")
-	w.Header().Set(headers.ContentType, "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(model.EncodeToJson(map[string]interface{}{
-		"podcasts":               podcasts,
-		"podcast_search_results": podcastSearchResults,
-	}))
+		w.Header().Set(headers.CacheControl, "private, max-age=3600")
+		w.Header().Set(headers.ContentType, "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(model.EncodeToJson(map[string]interface{}{
+			"podcasts":               podcasts,
+			"podcast_search_results": podcastSearchResults,
+		}))
+
+	} else if c.Params.Type == "episode" {
+		episodeSearchResults, err := c.App.SearchEpisodes(c.Params.Query, c.Params.SortBy, c.Params.Offset, c.Params.Limit)
+		if err != nil {
+			c.Err = err
+			return
+		}
+
+		episodeIds := make([]int64, len(episodeSearchResults))
+		for i, episodeSearchResult := range episodeSearchResults {
+			episodeIds[i] = episodeSearchResult.Id
+		}
+		episodes, err := c.App.GetEpisodesByIds(episodeIds)
+		if err != nil {
+			c.Err = err
+			return
+		}
+
+		podcastIds := make([]int64, len(episodes))
+		for i, episode := range episodes {
+			podcastIds[i] = episode.PodcastId
+		}
+		podcasts, err := c.App.GetPodcastsByIds(podcastIds)
+		if err != nil {
+			c.Err = err
+			return
+		}
+
+		w.Header().Set(headers.CacheControl, "private, max-age=3600")
+		w.Header().Set(headers.ContentType, "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(model.EncodeToJson(map[string]interface{}{
+			"podcasts":               podcasts,
+			"episodes":               episodes,
+			"episode_search_results": episodeSearchResults,
+		}))
+
+	} else {
+		c.SetInvalidQueryParam("type")
+	}
 }
