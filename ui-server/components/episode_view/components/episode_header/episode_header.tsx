be43c8eccbcce73a28f3d11c890778aa7c19847f
varmamsp <varmamsp111@gmail.com>
2020-01-09 21:32:12 +0530

    Add play button to episode page

--- ui-server/components/episode_view/components/episode_header/episode_header.tsx
+++ ui-server/components/episode_view/components/episode_header/episode_header.tsx
@@ -1,20 +1,45 @@
+import { iconMap } from 'components/icon'
 import { PodcastLink } from 'components/link'
+import format from 'date-fns/format'
 import React from 'react'
 import { Episode, Podcast } from 'types/app'
 import { getImageUrl } from 'utils/dom'
+import { formatEpisodeDuration } from 'utils/format'
 
 export interface StateToProps {
   podcast: Podcast
+}
+
+export interface DispatchToProps {
+  playEpisode: (stateTime: number) => void
+  showAddToPlaylistModal: () => void
 }
 
 export interface OwnProps {
   episode: Episode
 }
 
-const EpisodeHeader: React.FC<StateToProps & OwnProps> = ({
+type Props = StateToProps & DispatchToProps & OwnProps
+
+const EpisodeHeader: React.FC<Props> = ({
   episode,
   podcast,
+  playEpisode,
+  showAddToPlaylistModal,
 }) => {
+  let pubDate: string | undefined
+  try {
+    pubDate = format(new Date(`${episode.pubDate} +0000`), 'PP')
+  } catch (err) {}
+
+  let duration: string | undefined
+  if (episode.duration > 0) {
+    duration = formatEpisodeDuration(episode.duration)
+  }
+
+  const PlayIcon = iconMap['play']
+  const AddToPlaylistIcon = iconMap['playlist-add']
+
   return (
     <div className="flex">
       <img
@@ -32,6 +57,32 @@ const EpisodeHeader: React.FC<StateToProps & OwnProps> = ({
               {podcast.title}
             </a>
           </PodcastLink>
+
+          <div className="text-xs leading-relaxed tracking-wide text-gray-800">
+            {`Published on ${pubDate}`}
+            <span className="mx-2 font-extrabold">&middot;</span>
+            {duration}
+          </div>
+        </div>
+      
+        <div className="flex">
+          <button
+            className="flex items-center mr-4 px-3 py-1 text-2xs text-center bg-indigo-500 text-white border hover:border-2 rounded-lg focus:outline-none focus:shadow-outline"
+            onClick={() =>
+              playEpisode((episode.progress * episode.duration) / 100)
+            }
+          >
+            <PlayIcon className="fill-current w-4 h-auto" />
+            <span className="ml-1 font-medium">PLAY</span>
+          </button>
+
+          <button
+            className="flex items-center mr-4 px-3 py-1 text-2xs text-center text-gray-700 bg-gray-200 border rounded-lg focus:outline-none focus:shadow-outline"
+            onClick={() => showAddToPlaylistModal()}
+          >
+            <AddToPlaylistIcon className="fill-current w-4 h-auto" />
+            <span className="ml-2 font-medium">ADD</span>
+          </button>
         </div>
       </div>
     </div>
--- ui-server/components/episode_view/components/episode_header/index.ts
+++ ui-server/components/episode_view/components/episode_header/index.ts
@@ -1,7 +1,15 @@
+import { startPlayback } from 'actions/playback'
+import { loadAndShowAddToPlaylistModal } from 'actions/playlist'
 import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
 import { getPodcastById } from 'selectors/entities/podcasts'
 import { AppState } from 'store'
-import EpisodeHeader, { OwnProps, StateToProps } from './episode_header'
+import { AppActions } from 'types/actions'
+import EpisodeHeader, {
+  DispatchToProps,
+  OwnProps,
+  StateToProps,
+} from './episode_header'
 
 function mapStateToProps(state: AppState, { episode }: OwnProps): StateToProps {
   return {
@@ -9,6 +17,19 @@ function mapStateToProps(state: AppState, { episode }: OwnProps): StateToProps {
   }
 }
 
-export default connect<StateToProps, {}, OwnProps, AppState>(mapStateToProps)(
-  EpisodeHeader,
-)
+function mapDispatchToProps(
+  dispatch: Dispatch<AppActions>,
+  { episode }: OwnProps,
+): DispatchToProps {
+  return {
+    playEpisode: (startTime: number) =>
+      bindActionCreators(startPlayback, dispatch)(episode.id, startTime),
+    showAddToPlaylistModal: () =>
+      bindActionCreators(loadAndShowAddToPlaylistModal, dispatch)(episode.id),
+  }
+}
+
+export default connect<StateToProps, DispatchToProps, OwnProps, AppState>(
+  mapStateToProps,
+  mapDispatchToProps,
+)(EpisodeHeader)
