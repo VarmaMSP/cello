31adfd788bb16e23fb00b22008887ea8770c03e6
varmamsp <varmamsp111@gmail.com>
2020-01-10 06:55:10 +0530

    Add placeholder when there are no playlists.

--- ui-server/components/add_to_playlist_modal/add_to_playlist_modal.tsx
+++ ui-server/components/add_to_playlist_modal/add_to_playlist_modal.tsx
@@ -28,15 +28,22 @@ const AddToPlaylistModal: React.FC<Props> = ({
     <Overlay background="rgba(0, 0, 0, 0.65)">
       <ModalContainer className="modal-slim" header="Add to Playlist">
         <div className="h-full flex flex-col">
-          <div className="flex-1 overflow-y-auto">
-            {playlists.map((playlist) => (
-              <PlaylistsListItem
-                key={playlist.id}
-                playlist={playlist}
-                episodeId={episodeId}
-              />
-            ))}
-          </div>
+          {playlists.length === 0 ? (
+            <div className="flex-1 mt-5 text-center tracking-wider text-gray-800">
+              {'No playlists found, create one.'}
+            </div>
+          ) : (
+            <div className="flex-1 overflow-y-auto">
+              {playlists.map((playlist) => (
+                <PlaylistsListItem
+                  key={playlist.id}
+                  playlist={playlist}
+                  episodeId={episodeId}
+                />
+              ))}
+            </div>
+          )}
+
           <button
             className="block flex-none w-full text-sm font-medium text-center text-purple-700 py-1 mr-6 border-2 border-purple-600 rounded-lg"
             onClick={() => showCreatePlaylistModal()}
