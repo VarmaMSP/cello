08422b2d8dc34c50575245dbe4a4bdc4f6b94e22
varmamsp <varmamsp111@gmail.com>
2020-01-16 11:24:51 +0530

    Fix categories in smaller displays

--- ui-server/components/home_view/categories/categories.tsx
+++ ui-server/components/home_view/categories/categories.tsx
@@ -18,7 +18,7 @@ const Categories: React.FC<StateToProps> = ({ categories }) => {
               <li className="block flex items-center h-10 lg:w-1/2 pl-6 rounded-full hover:bg-gray-200">
                 <span className="text-lg text-gray-900">{c.title}</span>
                 &nbsp;&nbsp;
-                <span className="text-sm text-gray-700 tracking-wider">
+                <span className="md:inline hidden text-sm text-gray-700 tracking-wider">
                   {c.subTitle}
                 </span>
               </li>
