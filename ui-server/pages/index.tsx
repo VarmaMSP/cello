d119dfdbcd762104d2482d14d96366d4b82407a9
varmamsp <varmamsp111@gmail.com>
2020-01-24 11:39:17 +0530

    Stop showing sign in button in home page after user signed in.

--- ui-server/pages/index.tsx
+++ ui-server/pages/index.tsx
@@ -1,18 +1,26 @@
+import About from 'components/about'
 import { iconMap } from 'components/icon'
 import PageLayout from 'components/page_layout'
 import SignInButton from 'components/sign_in_button'
 import React from 'react'
+import { connect } from 'react-redux'
+import { getIsUserSignedIn } from 'selectors/session'
+import { AppState } from 'store'
 import { getAssetUrl } from 'utils/dom'
 
-const IndexPage: React.FC<{}> = () => {
+interface StateToProps {
+  isUserSignedIn: boolean
+}
+
+const IndexPage: React.FC<StateToProps> = ({ isUserSignedIn }) => {
   const LogoIcon = iconMap['logo-lg']
 
   return (
     <PageLayout>
-      <div className="flex md:flex-row-reverse flex-col -mb-32">
+      <div className="flex md:flex-row-reverse flex-col md:-mb-32">
         <img
           src={getAssetUrl('home')}
-          className="md:flex-1 md:w-5/12 md:-mt-8 -mt-56 -mr-6 "
+          className="md:flex-1 md:w-5/12 md:-mt-8 -mt-12 -mr-6 "
         />
         <div className="flex-none md:w-1/2 md:mt-0 -mt-10 md:pl-8 md:pt-16 ">
           <LogoIcon className="mb-2" />
@@ -35,13 +43,21 @@ const IndexPage: React.FC<{}> = () => {
           <p className="mb-8 tracking-wide text-xl text-teal-700 font-bold">
             {'Topic, Person, Podcast, Episode, Publisher...'}
           </p>
-          <p className="mb-5 tracking-wide break-words">
-            {
-              'Keep track of your subscriptions, listen history and curate episodes playlists. Sign Up / Sign In to get started.'
-            }
-          </p>
-          <div className="h-10 w-48">
-            <SignInButton />
+          {!isUserSignedIn && (
+            <>
+              <p className="mb-5 tracking-wide break-words">
+                {
+                  'Keep track of your subscriptions, listen history and curate episodes playlists. Sign Up / Sign In to get started.'
+                }
+              </p>
+              <div className="h-10 w-48">
+                <SignInButton />
+              </div>
+            </>
+          )}
+          <hr className="md:hidden mt-10 mb-6" />
+          <div className="md:hidden text-center">
+            <About />
           </div>
         </div>
       </div>
@@ -49,4 +65,12 @@ const IndexPage: React.FC<{}> = () => {
   )
 }
 
-export default IndexPage
+function mapStateToProps(state: AppState): StateToProps {
+  return {
+    isUserSignedIn: getIsUserSignedIn(state),
+  }
+}
+
+export default connect<StateToProps, {}, {}, AppState>(mapStateToProps)(
+  IndexPage,
+)
