ecdade9049c8ec02a4dcf06309bb067dc10aad6d
varmamsp <varmamsp111@gmail.com>
2020-01-25 23:40:22 +0530

    Add ability to customise episode thumbnail

--- ui-server/components/episode_thumbnail/episode_thumbnail.tsx
+++ ui-server/components/episode_thumbnail/episode_thumbnail.tsx
@@ -3,6 +3,7 @@ import { iconMap } from 'components/icon'
 import { Episode, Podcast } from 'types/app'
 import { getImageUrl } from 'utils/dom'
 import { formatDuration } from 'utils/format'
+
 export interface StateToProps {
   episode: Episode
   podcast: Podcast
@@ -14,6 +15,8 @@ export interface DispatchToProps {
 
 export interface OwnProps {
   episodeId: string
+  small: boolean
+  showIcon: boolean
 }
 
 type Props = StateToProps & DispatchToProps & OwnProps
@@ -22,13 +25,21 @@ const EpisodeThumbnail: React.FC<Props> = ({
   podcast,
   episode,
   playEpisode,
+  small,
+  showIcon,
 }) => {
   const PlayIcon = iconMap['play']
 
   return (
     <div>
       <div
-        className="relative md:w-28 w-24 md:h-28 h-24 mb-2 rounded-lg border cursor-pointer"
+        className={classnames(
+          'relative w-22 h-22 rounded-lg border cursor-pointer',
+          {
+            'md:w-25 md:h-25': !!small,
+            'md:w-28 md:h-28': !!!small,
+          },
+        )}
         onClick={() => {
           episode.progress >= 95
             ? playEpisode(0)
@@ -43,30 +54,45 @@ const EpisodeThumbnail: React.FC<Props> = ({
           )}
           src={getImageUrl(podcast.urlParam)}
         />
+
         {/* Icon */}
         <div
           className={classnames(
-            'absolute left-0 top-0 md:w-28 w-24 md:h-28 h-24',
-            'thumbnail-overlay',
+            'absolute left-0 top-0 w-22 h-22',
+            {
+              'md:w-25 md:h-25': !!small,
+              'md:w-28 md:h-28': !!!small,
+            },
+            {
+              overlay: showIcon,
+              'overlay-on-hover': !showIcon,
+            },
           )}
         >
           <div
-            className="flex items-center md:w-14 w-12 md:h-14 h-12 mx-auto rounded-full"
+            className="flex items-center md:w-14 md:h-14 w-10 h-10 mx-auto rounded-full"
             style={{ background: 'rgba(255, 255, 255, 0.75)' }}
           >
-            <PlayIcon className="md:w-10 w-8 md:h-10 h-8 ml-3 mx-auto fill-current text-gray-800" />
+            <PlayIcon className="md:w-10 md:h-10 w-7 h-7 md:ml-3 ml-2 fill-current text-gray-800" />
           </div>
         </div>
+
         {/* Duration */}
-        <div className="absolute right-0 bottom-0 px-2 text-2xs font-medium tracking-wide text-gray-800 bg-gray-300 rounded-lg">
+        <div
+          className="absolute right-0 bottom-0 px-1 text-2xs font-semibold text-gray-100 leading-tight rounded"
+          style={{ background: 'rgba(1, 1, 1, 0.8)' }}
+        >
           {formatDuration(episode.duration)}
         </div>
       </div>
 
       <div
-        className={classnames('relative w-full h-1 bg-gray-400 rounded-full', {
-          hidden: episode.lastPlayedAt === '',
-        })}
+        className={classnames(
+          'relative w-full h-1 mt-2 bg-gray-400 rounded-full',
+          {
+            hidden: episode.lastPlayedAt === '',
+          },
+        )}
       >
         <div
           className="absolute h-1 top-0 left-0 bg-green-700 rounded-full"
