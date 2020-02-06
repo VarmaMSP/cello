16aa6a444743b31271d43d25dab98c84707ea5e1
varmamsp <varmamsp111@gmail.com>
2020-02-07 03:04:51 +0530

    Use custom css to style page layout

--- ui-server/components/page_layout.tsx
+++ ui-server/components/page_layout.tsx
@@ -1,56 +1,20 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { getViewportSize } from 'selectors/window'
-import { AppState } from 'store'
-import { ViewportSize } from 'types/app'
-
-interface StateToProps {
-  viewPortSize: ViewportSize
-}
 
 interface OwnProps {
-  children: JSX.Element | JSX.Element[]
+  children: JSX.Element | [JSX.Element, JSX.Element]
 }
 
-const PageLayout: React.FC<StateToProps & OwnProps> = ({
-  viewPortSize,
+const PageLayout: React.FC<OwnProps> = ({
   children,
 }) => {
-  if (viewPortSize === 'SM') {
-    return Array.isArray(children) ? (
-      <div className="flex flex-col px-3 pt-14 pb-40">
-        {children[0]}
-        {children[1]}
-      </div>
-    ) : (
-      <div className="px-3 pt-14 pb-40">{children}</div>
-    )
-  }
-
   return Array.isArray(children) ? (
-    <div
-      className="flex flex-row justify-between pr-6"
-      style={{ paddingLeft: '18.5em', paddingBottom: '8rem' }}
-    >
-      <div style={{ width: '68%' }}>{children[0]}</div>
-      <div style={{ width: '28%' }}>{children[1]}</div>
+    <div className="page-layout-split">
+      <div className="first">{children[0]}</div>
+      <div className="second">{children[1]}</div>
     </div>
   ) : (
-    <div
-      className="pr-6"
-      style={{ paddingLeft: '18.5rem', paddingBottom: '6rem' }}
-    >
-      {children}
-    </div>
+    <div className="page-layout">{children}</div>
   )
 }
 
-function mapStateToProps(state: AppState): StateToProps {
-  return {
-    viewPortSize: getViewportSize(state),
-  }
-}
-
-export default connect<StateToProps, {}, OwnProps, AppState>(mapStateToProps)(
-  PageLayout,
-)
+export default PageLayout
