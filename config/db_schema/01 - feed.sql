34facfa0cccbc354182f00efce012319d7a8d849
varmamsp <varmamsp111@gmail.com>
2020-02-11 14:56:46 +0530

    Add new task to schema

--- config/db_schema/01 - feed.sql
+++ config/db_schema/01 - feed.sql
@@ -108,6 +108,7 @@ INSERT INTO `task` (`id`, `name`, `type`, `interval_`, `next_run_at`, `active`, 
 INSERT INTO `task` (`id`, `name`, `type`, `interval_`, `next_run_at`, `active`, `created_at`, `updated_at`) VALUES (7, 'reindex_podcasts', 'IMMEDIATE', 0, 0, 0, 0, 0);
 INSERT INTO `task` (`id`, `name`, `type`, `interval_`, `next_run_at`, `active`, `created_at`, `updated_at`) VALUES (8, 'fix_categories', 'IMMEDIATE', 0, 0, 0, 0, 0);
 INSERT INTO `task` (`id`, `name`, `type`, `interval_`, `next_run_at`, `active`, `created_at`, `updated_at`) VALUES (9, 'extract_keywords', 'IMMEDIATE', 0, 0, 0, 0, 0);
+INSERT INTO `task` (`id`, `name`, `type`, `interval_`, `next_run_at`, `active`, `created_at`, `updated_at`) VALUES (10, 'index_keywords', 'IMMEDIATE', 0, 0, 0, 0, 0);
 
 INSERT INTO `category` (`id`, `name`) VALUES (1, 'Arts');
 INSERT INTO `category` (`id`, `parent_id`, `name`) VALUES (2, 1, 'Books');
