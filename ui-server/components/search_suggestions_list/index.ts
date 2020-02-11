3ddfeca7e7f3eb06e082624ce17e63048bb8c502
varmamsp <varmamsp111@gmail.com>
2020-02-12 00:01:19 +0530

    Refactor suggestion list

--- ui-server/components/search_suggestions_list/index.ts
+++ ui-server/components/search_suggestions_list/index.ts
@@ -1,14 +1,46 @@
+import { loadPodcastPage, loadResultsPage } from 'actions/results'
 import { connect } from 'react-redux'
-import { getSuggestions } from 'selectors/ui/search_bar'
+import { bindActionCreators, Dispatch } from 'redux'
+import { getSuggestions, getText } from 'selectors/ui/search_bar'
 import { AppState } from 'store'
-import SearchSuggestionsList, { StateToProps } from './search_suggestions_list'
+import { AppActions } from 'types/actions'
+import { SearchSuggestion } from 'types/models'
+import SearchSuggestionsList, {
+  DispatchToProps,
+  StateToProps,
+} from './search_suggestions_list'
 
 function mapStateToProps(state: AppState): StateToProps {
+  const searchText = getText(state).trim()
+  const currentSuggestion = <SearchSuggestion>{
+    t: 'T',
+    i: 'C',
+    header: searchText,
+    subHeader: '',
+  }
+
   return {
-    suggestions: getSuggestions(state),
+    suggestions:
+      searchText.length > 0
+        ? [currentSuggestion, ...getSuggestions(state)]
+        : getSuggestions(state),
   }
 }
 
-export default connect<StateToProps, {}, {}, AppState>(mapStateToProps)(
-  SearchSuggestionsList,
-)
+function mapDispatchToProps(dispatch: Dispatch<AppActions>): DispatchToProps {
+  return {
+    loadResultsPage: (text: string) =>
+      bindActionCreators(loadResultsPage, dispatch)(
+        text,
+        'episode',
+        'relevance',
+      ),
+    loadPodcastPage: (podcastUrlParam: string) =>
+      bindActionCreators(loadPodcastPage, dispatch)(podcastUrlParam),
+  }
+}
+
+export default connect<StateToProps, DispatchToProps, {}, AppState>(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SearchSuggestionsList)
--- ui-server/components/search_suggestions_list/search_suggestions_list.tsx
+++ ui-server/components/search_suggestions_list/search_suggestions_list.tsx
@@ -1,58 +1,129 @@
-import { PodcastLink } from 'components/link'
-import React from 'react'
+import classnames from 'classnames'
+import { iconMap } from 'components/icon'
+import React, { useEffect, useState } from 'react'
 import { SearchSuggestion } from 'types/models'
-import { getImageUrl } from 'utils/dom'
-import { uniqueId } from 'utils/utils'
+import { getImageUrl, stopEventPropagation } from 'utils/dom'
 
 export interface StateToProps {
   suggestions: SearchSuggestion[]
 }
 
-const SearchSuggestionsList: React.FC<StateToProps> = ({ suggestions }) => {
-  if (suggestions.length === 0) {
-    return <></>
+export interface DispatchToProps {
+  loadResultsPage: (text: string) => void
+  loadPodcastPage: (podcastUrlParam: string) => void
+}
+
+const SearchSuggestionsList: React.FC<StateToProps & DispatchToProps> = ({
+  suggestions,
+  loadResultsPage,
+  loadPodcastPage,
+}) => {
+  const [cursor, setCursor] = useState<number>(1)
+
+  const handleSelect = (i: number) => () => {
+    if (SearchSuggestion.isPodcast(suggestions[i])) {
+      loadPodcastPage(suggestions[i].i)
+    } else {
+      loadResultsPage(suggestions[i].header)
+    }
   }
 
-  return (
+  const handleOnKeyDown = (e: any) => {
+    if (e.keyCode === 38 && cursor > 0) {
+      setCursor(cursor - 1)
+    } else if (e.keyCode === 40) {
+      setCursor(cursor + 1)
+    } else if (e.keyCode === 13) {
+      handleSelect(cursor)()
+    }
+  }
+
+  useEffect(() => {
+    cursor >= suggestions.length && setCursor(0)
+  }, [suggestions.length])
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleOnKeyDown)
+    return () => document.removeEventListener('keydown', handleOnKeyDown)
+  }, [cursor, suggestions.length])
+
+  return suggestions.length > 0 && suggestions[0].i === 'C' ? (
     <div
       style={{ width: '32rem' }}
       className="z-10 px-2 py-2 bg-white border border-blue-400 rounded-lg"
     >
-      {suggestions.map((s) => {
-        if (SearchSuggestion.isPodcast(s)) {
-          return renderPodcast(s, uniqueId())
-        }
-        return renderText(s, uniqueId())
-      })}
+      {suggestions.map((s, i) =>
+        SearchSuggestion.isPodcast(s)
+          ? renderItemSuggestion(s, handleSelect(i), cursor === i)
+          : renderTextSuggestion(s, handleSelect(i), cursor === i),
+      )}
+    </div>
+  ) : (
+    <></>
+  )
+}
+
+function renderTextSuggestion(
+  s: SearchSuggestion,
+  onClick: () => void,
+  active: boolean,
+): JSX.Element {
+  const Icon = iconMap[s.i === 'S' ? 'search' : 'enter']
+
+  return (
+    <div
+      key={`${s.i}-${s.t}-${s.header}`}
+      className={classnames(
+        'search-suggestion flex items-center px-3 py-1 hover:bg-gray-200 cursor-pointer rounded',
+        { 'bg-gray-200': active },
+      )}
+      onClick={onClick}
+      onPointerDown={stopEventPropagation}
+      onMouseDown={stopEventPropagation}
+      onTouchStart={stopEventPropagation}
+    >
+      <Icon className="flex-none w-4 h-4 mr-4 fill-current text-gray-800" />
+      <div
+        className="lowercase text-base text-gray-900 leading-loose line-clamp-1"
+        dangerouslySetInnerHTML={{ __html: s.header }}
+      />
     </div>
   )
 }
 
-const renderPodcast = (p: SearchSuggestion, key: string): JSX.Element => (
-  <PodcastLink key={key} podcastUrlParam={p.i}>
-    <a className="search-suggestion block flex px-3 py-3 hover:bg-gray-200 rounded">
-      <img src={getImageUrl(p.i)} className="w-12 h-12 mr-3 border rounded" />
+function renderItemSuggestion(
+  s: SearchSuggestion,
+  onClick: () => void,
+  active: boolean,
+): JSX.Element {
+  return (
+    <div
+      key={`${s.i}-${s.t}-${s.header}`}
+      className={classnames(
+        'search-suggestion flex p-3 hover:bg-gray-200 cursor-pointer rounded',
+        { 'bg-gray-200': active },
+      )}
+      onClick={onClick}
+      onPointerDown={stopEventPropagation}
+      onMouseDown={stopEventPropagation}
+      onTouchStart={stopEventPropagation}
+    >
+      <img
+        src={getImageUrl(s.i)}
+        className="flex-none w-12 h-12 mr-3 border rounded"
+      />
       <div>
         <div
           className="text-base text-gray-900 line-clamp-1"
-          dangerouslySetInnerHTML={{ __html: p.header }}
+          dangerouslySetInnerHTML={{ __html: s.header }}
         />
         <div
           className="text-sm text-gray-900 line-clamp-1"
-          dangerouslySetInnerHTML={{ __html: p.subHeader }}
+          dangerouslySetInnerHTML={{ __html: s.subHeader }}
         />
       </div>
-    </a>
-  </PodcastLink>
-)
-
-const renderText = (t: SearchSuggestion, key: string): JSX.Element => (
-  <div key={key}>
-    <div
-      className="text-base text-gray-900 line-clamp-1"
-      dangerouslySetInnerHTML={{ __html: t.header }}
-    />
-  </div>
-)
+    </div>
+  )
+}
 
 export default SearchSuggestionsList
