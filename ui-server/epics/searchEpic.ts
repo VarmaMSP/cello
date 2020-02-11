e6359715c18436243c6935c59a9316da3f316d83
varmamsp <varmamsp111@gmail.com>
2020-02-11 23:57:59 +0530

    Decrease debounce time

--- ui-server/epics/searchEpic.ts
+++ ui-server/epics/searchEpic.ts
@@ -17,7 +17,7 @@ const searchEpic: Epic<T.AppActions, T.AppActions, AppState> = (action$) =>
   action$.pipe(
     ofType(T.SEARCH_BAR_UPDATE_TEXT),
     filter<UpdateTextAction>(({ text }) => text.trim().length > 0),
-    debounceTime<UpdateTextAction>(350),
+    debounceTime<UpdateTextAction>(300),
     concatMap<UpdateTextAction, Observable<T.AppActions>>((action) =>
       from(
         doFetch({
