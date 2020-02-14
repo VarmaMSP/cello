affaee1a09255db8b6fc450e33d7153d00b2b895
varmamsp <varmamsp111@gmail.com>
2020-02-14 08:51:03 +0530

    Finishing touches to home view

--- ui-server/components/home_view/home_view.tsx
+++ ui-server/components/home_view/home_view.tsx
@@ -1,3 +1,5 @@
+import classnames from 'classnames'
+import Grid from 'components/grid'
 import { PodcastLink } from 'components/link'
 import SignInButton from 'components/sign_in_button'
 import React from 'react'
@@ -42,36 +44,55 @@ const acclaimedPodcasts = [
   },
 ]
 
-const HomeView: React.FC<{}> = () => {
+export interface StateToProps {
+  isUserSignedIn: boolean
+}
+
+const HomeView: React.FC<StateToProps> = ({ isUserSignedIn }) => {
   return (
     <div>
       <div className="flex md:flex-row flex-col pt-6">
-        <div className="flex-1 pt-16">
+        <div className="flex-1 md:pt-16">
           <h1 className="text-5xl text-center text-purple-700 font-semibold tracking-wide">
             {'Phenopod'}
           </h1>
           <h2 className="text-lg text-center text-gray-900 font-medium leading-relaxed tracking-wide">
             {'Simple, Yet Powerful Podcast Player'}
           </h2>
-          <h3 className="w-3/4 mt-6 mx-auto text-sm text-center text-gray-800 font-medium tracking-wide">
+          <h3
+            className={classnames(
+              'w-3/4 mt-6 mx-auto text-sm text-center text-gray-800 font-medium tracking-wide',
+              { hidden: isUserSignedIn },
+            )}
+          >
             {
               'Discover, Subscribe, Curate Your Favourite Podcasts And Episodes. Sign In Now To Get Started.'
             }
           </h3>
-          <div className="w-56 h-8 mx-auto mt-3">
+          <div
+            className={classnames('w-56 h-8 mx-auto mt-3', {
+              hidden: isUserSignedIn,
+            })}
+          >
             <SignInButton />
           </div>
         </div>
-        <img src={getAssetUrl('listen-to-podcasts.svg')} className="w-1/2" />
+        <img
+          src={getAssetUrl('listen-to-podcasts.svg')}
+          className="md:block hidden w-1/2"
+        />
       </div>
 
-      <div className="flex mt-12 pl-6">
-        <img src={getAssetUrl('powerful-search.png')} className="w-1/3" />
-        <div className="flex-1 pt-14">
+      <div className="flex md:flex-row flex-col mt-12 md:pl-6">
+        <img
+          src={getAssetUrl('powerful-search.png')}
+          className="md:w-1/3 w-3/5 md:mx-0 mx-auto"
+        />
+        <div className="flex-1 md:pt-14 pt-6">
           <h1 className="text-2xl text-center text-black font-medium leading-relaxed tracking-wide">
             {'Powerful Search Engine'}
           </h1>
-          <h2 className="w-3/4 mt-3 mx-auto text-lg text-center text-gray-800 font-medium leading-relaxed tracking-wide">
+          <h2 className="md:w-3/4 w-full mt-3 mx-auto text-lg text-center text-gray-800 font-medium leading-relaxed tracking-wide">
             {
               'Search For Person, Topic, Place, Podcast, Episode.. From A Directory Of 638,417 Podcasts And 21,07,559 Episodes.'
             }
@@ -79,13 +100,16 @@ const HomeView: React.FC<{}> = () => {
         </div>
       </div>
 
-      <div className="flex mt-16 pl-6">
-        <img src={getAssetUrl('subscriptions-feed.png')} className="w-1/3" />
-        <div className="flex-1 pt-12">
+      <div className="flex md:flex-row flex-col mt-16 md:pl-6">
+        <img
+          src={getAssetUrl('subscriptions-feed.png')}
+          className="md:w-1/3 w-3/5 md:mx-0 mx-auto"
+        />
+        <div className="flex-1 md:pt-12 pt-6">
           <h1 className="text-2xl text-center text-black font-medium leading-relaxed tracking-wide">
             {'Subscriptions'}
           </h1>
-          <h2 className="w-3/4 mt-3 mx-auto text-lg text-center text-gray-800 font-medium leading-relaxed tracking-wide">
+          <h2 className="md:w-3/4 w-full mt-3 mx-auto text-lg text-center text-gray-800 font-medium leading-relaxed tracking-wide">
             {
               'Subscribe To Your Favourite Podcasts And Get Lastest Episodes In Your Subscriptions Feed.'
             }
@@ -93,13 +117,16 @@ const HomeView: React.FC<{}> = () => {
         </div>
       </div>
 
-      <div className="flex mt-16 pl-6">
-        <img src={getAssetUrl('sync-progress.png')} className="w-1/3" />
-        <div className="flex-1 pt-14">
+      <div className="flex md:flex-row flex-col mt-16 md:pl-6">
+        <img
+          src={getAssetUrl('sync-progress.png')}
+          className="md:w-1/3 w-3/5 md:mx-0 mx-auto"
+        />
+        <div className="flex-1 md:pt-12 pt-6">
           <h1 className="text-2xl text-center text-black font-medium leading-relaxed tracking-wide">
             {'Never Loose Your Progress'}
           </h1>
-          <h2 className="w-3/4 mt-3 mx-auto text-lg text-center text-gray-800 font-medium leading-relaxed tracking-wide">
+          <h2 className="md:w-3/4 w-full mt-3 mx-auto text-lg text-center text-gray-800 font-medium leading-relaxed tracking-wide">
             {
               'Your Progress Gets Automatically Saved, So Just Pick Up Your Favourite Episode From Where You Left.'
             }
@@ -107,32 +134,32 @@ const HomeView: React.FC<{}> = () => {
         </div>
       </div>
 
-      <h1 className="mt-8 text-center font-medium tracking-wide">
-        <span className="text-xl">
-          {'Not Able to Decide Where To Start ? '}
-        </span>
+      <h1 className="md:mt-8 mt-24 text-center font-medium tracking-wide">
+        <span className="text-xl">{'Not Sure Where To Start ? '}</span>
         <span className="text-lg">
           {'Check Out These Critically Acclaimed Podcasts'}
         </span>
       </h1>
       <div className="flex mt-6">
-        {acclaimedPodcasts.map((p) => (
-          <div key={p.id} className="flex-none w-1/6 px-5 mb-4">
-            <PodcastLink podcastUrlParam={p.urlParam}>
-              <a>
-                <img
-                  className="w-full h-auto mb-2 flex-none object-contain rounded-lg border"
-                  src={getImageUrl(p.urlParam)}
-                />
-              </a>
-            </PodcastLink>
-            <PodcastLink podcastUrlParam={p.urlParam}>
-              <a className="text-xs text-gray-900 tracking-wide font-medium leading-snug line-clamp-2">
-                {p.title}
-              </a>
-            </PodcastLink>
-          </div>
-        ))}
+        <Grid cols={{ SM: 3, MD: 6, LG: 6 }}>
+          {acclaimedPodcasts.map((p) => (
+            <div key={p.id} className="flex-none md:px-5 px-3 mb-4">
+              <PodcastLink podcastUrlParam={p.urlParam}>
+                <a>
+                  <img
+                    className="w-full h-auto mb-2 flex-none object-contain rounded-lg border"
+                    src={getImageUrl(p.urlParam)}
+                  />
+                </a>
+              </PodcastLink>
+              <PodcastLink podcastUrlParam={p.urlParam}>
+                <a className="text-xs text-gray-900 tracking-wide font-medium leading-snug line-clamp-2">
+                  {p.title}
+                </a>
+              </PodcastLink>
+            </div>
+          ))}
+        </Grid>
       </div>
     </div>
   )
--- ui-server/pages/index.tsx
+++ ui-server/pages/index.tsx
@@ -1,16 +1,9 @@
-import HomeView from 'components/home_view/home_view'
+import HomeView from 'components/home_view'
 import PageLayout from 'components/page_layout'
 import { IndexPageSeo } from 'components/seo'
 import React from 'react'
-import { connect } from 'react-redux'
-import { getIsUserSignedIn } from 'selectors/session'
-import { AppState } from 'store'
 
-interface StateToProps {
-  isUserSignedIn: boolean
-}
-
-const IndexPage: React.FC<StateToProps> = () => {
+const IndexPage: React.FC<{}> = () => {
   return (
     <>
       <IndexPageSeo />
@@ -21,12 +14,4 @@ const IndexPage: React.FC<StateToProps> = () => {
   )
 }
 
-function mapStateToProps(state: AppState): StateToProps {
-  return {
-    isUserSignedIn: getIsUserSignedIn(state),
-  }
-}
-
-export default connect<StateToProps, {}, {}, AppState>(mapStateToProps)(
-  IndexPage,
-)
+export default IndexPage
