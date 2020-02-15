6627344b04c346784498fbafe31dd65bea61dd7e
varmamsp <varmamsp111@gmail.com>
2020-02-15 21:53:39 +0530

    Add some queries for analytics

--- config/db_schema/analytics.sql
+++ config/db_schema/analytics.sql
@@ -1,14 +1,36 @@
--- USERS
-SELECT id, name, email, CONVERT_TZ(FROM_UNIXTIME(created_at), "+00:00", "+05:30") AS joined_at FROM user ORDER BY joined_at;
-SELECT DATE(CONVERT_TZ(FROM_UNIXTIME(created_at), "+00:00", "+05:30")) AS joined_date, count(*) AS users_registered FROM user GROUP BY joined_date;
-
--- SUBSCRIPTIONS
-SELECT user.name, COUNT(subscription.podcast_id) AS subscriptions FROM user INNER JOIN subscription ON subscription.user_id = user.id GROUP BY user.name ORDER BY subscriptions;
-SELECT podcast.title FROM podcast INNER JOIN subscription ON subscription.podcast_id = podcast.id WHERE subscription.user_id = '';
-
--- EPISODE PLAYBACK
-SELECT episode.title, user.name AS user_id, playback.current_progress AS played_till, CONVERT_TZ(playback.last_played_at, "+00:00", "+05:30") AS played_at FROM playback INNER JOIN episode ON episode.id = playback.episode_id INNER JOIN user ON user.id = playback.user_id WHERE user.id = 5;
-SELECT user.name, COUNT(playback.episode_id) AS playbacks FROM user INNER JOIN playback ON playback.user_id = user.id GROUP BY user.name ORDER BY playbacks;
-
 -- TASKS
-SELECT name, type, `interval`, CONVERT_TZ(FROM_UNIXTIME(next_run_at), "+00:00", "+05:30") AS next_run_at, CONVERT_TZ(FROM_UNIXTIME(updated_at), "+00:00", "+05:30") AS updated_at from task; 
+SELECT name, type, interval_,
+    CONVERT_TZ(FROM_UNIXTIME(next_run_at), "+00:00", "+05:30") AS next_run_at,
+    CONVERT_TZ(FROM_UNIXTIME(updated_at), "+00:00", "+05:30") AS last_run_at from task;
+
+-- USER ACTIVITY
+SELECT id, name, email, CONVERT_TZ(FROM_UNIXTIME(created_at), "+00:00", "+05:30") AS joined_at FROM user
+    ORDER BY joined_at;
+
+-- NO OF SIGN UPS PER DAY
+SELECT DATE(CONVERT_TZ(FROM_UNIXTIME(created_at), "+00:00", "+05:30")) AS joined_date, count(*) AS users_registered FROM user
+    GROUP BY joined_date;
+
+-- NO OF SUBSCRIPTIOS PER USER
+SELECT user.name, COUNT(subscription.podcast_id) AS subscriptions FROM user
+    INNER JOIN subscription ON subscription.user_id = user.id
+    GROUP BY user.name
+    ORDER BY subscriptions;
+
+-- NO OF PLAYBACKS PER USER
+SELECT user.name, COUNT(playback.episode_id) AS playbacks FROM user
+    INNER JOIN playback ON playback.user_id = user.id
+    GROUP BY user.name
+    ORDER BY playbacks;
+
+-- USER SUBSCRIPTIONS
+SELECT user.name, podcast.title FROM subscription
+    INNER JOIN user ON subscription.user_id = user.id
+    INNER JOIN podcast ON subscription.podcast_id = podcast.id
+    WHERE subscription.user_id = '';
+
+-- USER PLAYBACKS
+SELECT user.name, episode.title FROM playback
+    INNER JOIN user ON playback.user_id = user.id
+    INNER JOIN episode ON playback.episode_id = episode.id
+    WHERE playback.user_id = '';
