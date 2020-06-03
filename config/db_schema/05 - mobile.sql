3a7cc84edc6c62bd3571bcdb80d280c8789d4672
varmamsp <varmamsp111@gmail.com>
2020-06-03 15:27:38 +0530

    Change sign_in_method column type

--- config/db_schema/05 - mobile.sql
+++ config/db_schema/05 - mobile.sql
@@ -11,3 +11,5 @@ CREATE TABLE `guest_account` (
     PRIMARY KEY(`id`),
     FOREIGN KEY(`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE 
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE `user` MODIFY COLUMN `sign_in_method` enum('EMAIL','GOOGLE','FACEBOOK','TWITTER', 'GUEST');
